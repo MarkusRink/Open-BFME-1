f2e0c6e1a5de0350fe13be85fa725a0cfd23350c
Jonathan <jonathan@users.noreply.github.com>
2026-09-06 17:52:03 -0400

    Replace misidentified DX8 assembly lift with real HTTP post cleanup

--- Code/GameEngine/Source/GameNetwork/GameSpy/ghttp/ghttpPost.c
+++ Code/GameEngine/Source/GameNetwork/GameSpy/ghttp/ghttpPost.c
@@ -1,4 +1,4 @@
-// cl: /DNDEBUG /MD -Ireference/shims/gamespy
+// cl: /ICode/GameEngine/Source/GameNetwork/GameSpy/ghttp /DNDEBUG /MD -Ireference/shims/gamespy
 /* GameSpy SDK, 2004 vintage -- pristine upstream C source.
    Sourced from the Area 51 (Inevitable Entertainment / Midway) source release,
    github.com/bisc67/Area51, Support/NetworkMgr/GameSpy -- the only public
