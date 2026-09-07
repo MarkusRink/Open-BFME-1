f9fb6187e34d99a39795cd3cabc247208eb7263d
Gabe Borges <gbborges@proton.me>
2026-09-07 06:33:45 -0400

    feat(common): convert OS display warning box
    The existing OSDisplayWarningBox symbol, game-text vtable slot 0x24, and matching MessageBox and SetWindowPos imports prove the identity. The clean body stays in Code/GameEngine/Source/Common/OSDisplayWarningBox_Thunk.cpp and reproduces all 509 retail bytes.

--- Code/GameEngine/Source/Common/OSDisplayWarningBox_Thunk.cpp
+++ Code/GameEngine/Source/Common/OSDisplayWarningBox_Thunk.cpp
@@ -1,521 +1,154 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// OSDisplayWarningBox at retail RVA 0x006BB040, 509 bytes.
 
-enum OSDisplayButtonType {};
-class AsciiString {};
-OSDisplayButtonType OSDisplayWarningBox(AsciiString, AsciiString, unsigned int, unsigned int);
-// ?OSDisplayWarningBox@@YA?AW4OSDisplayButtonType@@VAsciiString@@0II@Z
-__declspec(naked) OSDisplayButtonType OSDisplayWarningBox(AsciiString, AsciiString, unsigned int, unsigned int)
+typedef unsigned int UnsignedInt;
+typedef unsigned short WideChar;
+
+template <typename T> struct StringInlineData
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0x10
-		__emit 0x96
-		__emit 0x04
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xec
-		__emit 0x14
-		__emit 0x53
-		__emit 0x56
-		__emit 0xa1
-		__emit 0x7c
-		__emit 0x14
-		__emit 0x2f
-		__emit 0x01
-		__emit 0x33
-		__emit 0xdb
-		__emit 0x3b
-		__emit 0xc3
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x24
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x75
-		__emit 0x34
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x2c
-		__emit 0x88
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x24
-		__emit 0xe8
-		__emit 0xc6
-		__emit 0xc8
-		__emit 0x1c
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x30
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x24
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0xb5
-		__emit 0xc8
-		__emit 0x1c
-		__emit 0x00
-		__emit 0xb8
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x80
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x1c
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x5e
-		__emit 0x5b
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x20
-		__emit 0xc3
-		__emit 0x53
-		__emit 0x51
-		__emit 0x8d
-		__emit 0x44
-		__emit 0x24
-		__emit 0x34
-		__emit 0x89
-		__emit 0x64
-		__emit 0x24
-		__emit 0x20
-		__emit 0x8b
-		__emit 0xcc
-		__emit 0x50
-		__emit 0xe8
-		__emit 0xad
-		__emit 0xca
-		__emit 0x1c
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x7c
-		__emit 0x14
-		__emit 0x2f
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x11
-		__emit 0x8d
-		__emit 0x44
-		__emit 0x24
-		__emit 0x14
-		__emit 0x50
-		__emit 0xff
-		__emit 0x52
-		__emit 0x24
-		__emit 0x53
-		__emit 0x51
-		__emit 0x8d
-		__emit 0x54
-		__emit 0x24
-		__emit 0x38
-		__emit 0x89
-		__emit 0x64
-		__emit 0x24
-		__emit 0x20
-		__emit 0x8b
-		__emit 0xcc
-		__emit 0x52
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x30
-		__emit 0x02
-		__emit 0xe8
-		__emit 0x86
-		__emit 0xca
-		__emit 0x1c
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x7c
-		__emit 0x14
-		__emit 0x2f
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x54
-		__emit 0x24
-		__emit 0x10
-		__emit 0x52
-		__emit 0xff
-		__emit 0x50
-		__emit 0x24
-		__emit 0x8b
-		__emit 0x44
-		__emit 0x24
-		__emit 0x38
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x34
-		__emit 0x50
-		__emit 0x51
-		__emit 0x8d
-		__emit 0x44
-		__emit 0x24
-		__emit 0x18
-		__emit 0x89
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x18
-		__emit 0xe8
-		__emit 0xcf
-		__emit 0xfe
-		__emit 0xff
-		__emit 0xff
-		__emit 0xa0
-		__emit 0x28
-		__emit 0xd5
-		__emit 0x2e
-		__emit 0x01
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x08
-		__emit 0x3a
-		__emit 0xc3
-		__emit 0x74
-		__emit 0x4a
-		__emit 0x8b
-		__emit 0x44
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x3b
-		__emit 0xc3
-		__emit 0x8d
-		__emit 0x48
-		__emit 0x08
-		__emit 0x75
-		__emit 0x05
-		__emit 0xb9
-		__emit 0x8c
-		__emit 0x38
-		__emit 0x07
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x44
-		__emit 0x24
-		__emit 0x08
-		__emit 0x3b
-		__emit 0xc3
-		__emit 0x74
-		__emit 0x18
-		__emit 0x8b
-		__emit 0x54
-		__emit 0x24
-		__emit 0x10
-		__emit 0x52
-		__emit 0x51
-		__emit 0x83
-		__emit 0xc0
-		__emit 0x08
-		__emit 0x50
-		__emit 0x53
-		__emit 0xff
-		__emit 0x15
-		__emit 0x40
-		__emit 0x90
-		__emit 0x35
-		__emit 0x01
-		__emit 0x8b
-		__emit 0xf0
-		__emit 0xe9
-		__emit 0xa6
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x54
-		__emit 0x24
-		__emit 0x10
-		__emit 0x52
-		__emit 0x51
-		__emit 0xb8
-		__emit 0x8c
-		__emit 0x38
-		__emit 0x07
-		__emit 0x01
-		__emit 0x50
-		__emit 0x53
-		__emit 0xff
-		__emit 0x15
-		__emit 0x40
-		__emit 0x90
-		__emit 0x35
-		__emit 0x01
-		__emit 0x8b
-		__emit 0xf0
-		__emit 0xe9
-		__emit 0x8c
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x89
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x18
-		__emit 0x89
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x14
-		__emit 0x8d
-		__emit 0x44
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x50
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x1c
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x28
-		__emit 0x05
-		__emit 0xe8
-		__emit 0xde
-		__emit 0xde
-		__emit 0x1c
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0x51
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x18
-		__emit 0xe8
-		__emit 0xd0
-		__emit 0xde
-		__emit 0x1c
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x15
-		__emit 0x38
-		__emit 0xd2
-		__emit 0x2e
-		__emit 0x01
-		__emit 0x6a
-		__emit 0x03
-		__emit 0x53
-		__emit 0x53
-		__emit 0x53
-		__emit 0x53
-		__emit 0x6a
-		__emit 0xfe
-		__emit 0x52
-		__emit 0xff
-		__emit 0x15
-		__emit 0x84
-		__emit 0x90
-		__emit 0x35
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x44
-		__emit 0x24
-		__emit 0x18
-		__emit 0x3b
-		__emit 0xc3
-		__emit 0x8d
-		__emit 0x48
-		__emit 0x08
-		__emit 0x75
-		__emit 0x05
-		__emit 0xb9
-		__emit 0x8b
-		__emit 0x38
-		__emit 0x07
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x44
-		__emit 0x24
-		__emit 0x14
-		__emit 0x3b
-		__emit 0xc3
-		__emit 0x74
-		__emit 0x05
-		__emit 0x83
-		__emit 0xc0
-		__emit 0x08
-		__emit 0xeb
-		__emit 0x05
-		__emit 0xb8
-		__emit 0x8b
-		__emit 0x38
-		__emit 0x07
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x54
-		__emit 0x24
-		__emit 0x10
-		__emit 0x52
-		__emit 0x51
-		__emit 0x50
-		__emit 0x53
-		__emit 0xff
-		__emit 0x15
-		__emit 0x3c
-		__emit 0x90
-		__emit 0x35
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0x8b
-		__emit 0xf0
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x24
-		__emit 0x04
-		__emit 0xe8
-		__emit 0x6b
-		__emit 0xc7
-		__emit 0x1c
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x18
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x24
-		__emit 0x03
-		__emit 0xe8
-		__emit 0x5d
-		__emit 0xc7
-		__emit 0x1c
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x24
-		__emit 0x02
-		__emit 0xe8
-		__emit 0xdf
-		__emit 0xcf
-		__emit 0x1c
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x0c
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x24
-		__emit 0x01
-		__emit 0xe8
-		__emit 0xd1
-		__emit 0xcf
-		__emit 0x1c
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x2c
-		__emit 0x88
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x24
-		__emit 0xe8
-		__emit 0x34
-		__emit 0xc7
-		__emit 0x1c
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x30
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x24
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x23
-		__emit 0xc7
-		__emit 0x1c
-		__emit 0x00
-		__emit 0x83
-		__emit 0xfe
-		__emit 0x01
-		__emit 0xb8
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x74
-		__emit 0x05
-		__emit 0xb8
-		__emit 0x02
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x1c
-		__emit 0x5e
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x5b
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x20
-		__emit 0xc3
-	}
+    int m_refCount;
+    int m_length;
+    T m_text[1];
+};
+
+template <typename T> class StringBase
+{
+    friend class AsciiString;
+    friend class UnicodeString;
+
+private:
+    StringBase() : m_data( 0 ) {}
+    StringBase( const T *text );
+    StringBase( const StringBase<T> &other );
+    ~StringBase();
+    void set( const StringBase<T> &other );
+
+    StringInlineData<T> *m_data;
+};
+
+class UnicodeString;
+
+extern const char Rva006A16B0Empty[];
+extern const char g_bfmeEmptyUnicode[];
+
+class AsciiString : private StringBase<char>
+{
+public:
+    AsciiString() : StringBase<char>() {}
+    AsciiString( const AsciiString &other ) : StringBase<char>( other ) {}
+    ~AsciiString() {}
+
+    const char *str() const
+    {
+        return m_data ? m_data->m_text : Rva006A16B0Empty;
+    }
+
+    void translate( const UnicodeString &other );
+};
+
+class UnicodeString : private StringBase<WideChar>
+{
+public:
+    UnicodeString() : StringBase<WideChar>() {}
+    UnicodeString( const WideChar *text ) : StringBase<WideChar>( text ) {}
+    UnicodeString( const UnicodeString &other ) : StringBase<WideChar>( other ) {}
+    ~UnicodeString() {}
+
+    UnicodeString &operator=( const UnicodeString &other )
+    {
+        StringBase<WideChar>::set( *(const StringBase<WideChar> *)&other );
+        return *this;
+    }
+
+    const WideChar *str() const
+    {
+        return m_data ? m_data->m_text : (const WideChar *)g_bfmeEmptyUnicode;
+    }
+
+    void __cdecl format( UnicodeString format, ... );
+};
+
+class GameTextInterface
+{
+public:
+    virtual void slot00();
+    virtual void slot01();
+    virtual void slot02();
+    virtual void slot03();
+    virtual void slot04();
+    virtual void slot05();
+    virtual void slot06();
+    virtual void slot07();
+    virtual void slot08();
+    virtual UnicodeString fetch( AsciiString label, bool *exists = 0 );
+};
+
+extern GameTextInterface *TheGameText;
+extern const bool TheSystemIsUnicode;
+static void RTSFlagsToOSFlags( UnsignedInt buttonFlags, UnsignedInt otherFlags,
+    UnsignedInt &outWindowsFlags )
+{
+    outWindowsFlags = 0;
+    if ( buttonFlags & 2 )
+        outWindowsFlags |= 1;
+    if ( buttonFlags & 1 )
+        outWindowsFlags |= 0x1000;
+    if ( otherFlags & 4 )
+        outWindowsFlags |= 0x2000;
+    if ( otherFlags & 8 )
+        outWindowsFlags |= 0x30;
+    if ( otherFlags & 0x10 )
+        outWindowsFlags |= 0x40;
+    if ( otherFlags & 0x11 )
+        outWindowsFlags |= 0x10;
+    if ( otherFlags & 0x12 )
+        outWindowsFlags |= 0x10;
+}
+extern void *Rva0048CD50WindowHandle;
+
+extern "C" __declspec( dllimport ) int __stdcall MessageBoxW(
+    void *window, const WideChar *message, const WideChar *caption, UnsignedInt flags );
+extern "C" __declspec( dllimport ) int __stdcall MessageBoxA(
+    void *window, const char *message, const char *caption, UnsignedInt flags );
+extern "C" __declspec( dllimport ) int __stdcall SetWindowPos(
+    void *window, int insertAfter, int x, int y, int width, int height, UnsignedInt flags );
+
+enum OSDisplayButtonType
+{
+    OSDBT_OK = 1,
+    OSDBT_CANCEL = 2,
+    OSDBT_ERROR = 0x80000000
+};
+
+// ?OSDisplayWarningBox@@YA?AW4OSDisplayButtonType@@VAsciiString@@0II@Z
+OSDisplayButtonType OSDisplayWarningBox( AsciiString p, AsciiString m,
+    UnsignedInt buttonFlags, UnsignedInt otherFlags )
+{
+    if ( TheGameText == 0 )
+        return OSDBT_ERROR;
+
+    UnicodeString promptStr = TheGameText->fetch( p );
+    UnicodeString mesgStr = TheGameText->fetch( m );
+
+    UnsignedInt windowsOptionsFlags = 0;
+    RTSFlagsToOSFlags( buttonFlags, otherFlags, windowsOptionsFlags );
+
+    int returnResult = 0;
+    if ( TheSystemIsUnicode )
+    {
+        returnResult = MessageBoxW( 0, mesgStr.str(), promptStr.str(), windowsOptionsFlags );
+    }
+    else
+    {
+        AsciiString promptA;
+        AsciiString mesgA;
+        promptA.translate( promptStr );
+        mesgA.translate( mesgStr );
+        SetWindowPos( Rva0048CD50WindowHandle, -2, 0, 0, 0, 0, 3 );
+        returnResult = MessageBoxA( 0, mesgA.str(), promptA.str(), windowsOptionsFlags );
+    }
+
+    if ( returnResult == 1 )
+        return OSDBT_OK;
+    return OSDBT_CANCEL;
 }
