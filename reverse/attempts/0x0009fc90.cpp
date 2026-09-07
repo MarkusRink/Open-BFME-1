28947b038d9bce0687b7087d471b26d8bd907016
TheThag <zackgreco@hotmail.com>
2026-09-07 07:59:15 -0300

    Luna36h: bank SkirmishPreferences serializer near-match

---
+++ reverse/attempts/0x0009fc90.cpp
@@ -0,0 +1,158 @@
+// ?Rva0009FC90@Gen0009FC90Owner@@QAEXXZ
+// partial score=0.96 date=2026-09-07
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS
+// stlport
+//
+// Retail 0x0009FC90 is the UserNames-list serializer reached by the real
+// SkirmishPreferences write slot at 0x0009FEB0 (ILT 0x0002CB10).  The public
+// helper name is not recoverable from the callers, so the owner and method
+// below are deliberately address-derived.  The constructor at 0x0009F850
+// proves the list member at this+0x14 and its UnicodeString node payload.
+
+typedef bool Bool;
+typedef unsigned short WideChar;
+
+template <class T> struct StringInlineData
+{
+	unsigned short m_refCount;
+	unsigned short m_reserved;
+	unsigned short m_length;
+	unsigned short m_pad;
+	T m_text[1];
+};
+
+class AsciiString;
+class UnicodeString;
+
+template <class T> class StringBase
+{
+	friend class AsciiString;
+	friend class UnicodeString;
+
+public:
+	StringBase(void) : m_data(0) {}
+	void concat(const T *text, int length);
+
+private:
+	StringBase(const T *text);
+	StringBase(const StringBase<T> &other);
+	~StringBase();
+
+	void set(const StringBase<T> &other);
+	void releaseBuffer(void);
+
+	StringInlineData<T> *m_data;
+};
+
+class AsciiString : private StringBase<char>
+{
+public:
+	AsciiString(const char *text) : StringBase<char>(text) {}
+	AsciiString(const AsciiString &other) : StringBase<char>(other) {}
+	~AsciiString();
+
+	AsciiString &operator=(const AsciiString &other)
+	{
+		StringBase<char>::set(other);
+		return *this;
+	}
+};
+
+class UnicodeString : private StringBase<WideChar>
+{
+public:
+	UnicodeString(void) : StringBase<WideChar>() {}
+	UnicodeString(const UnicodeString &other)
+		: StringBase<WideChar>(other) {}
+	~UnicodeString() {}
+
+	int getLength(void) const
+	{
+		StringInlineData<WideChar> *data =
+			*(StringInlineData<WideChar> *const *)this;
+		return data ? data->m_length : 0;
+	}
+
+	const WideChar *str(void) const
+	{
+		StringInlineData<WideChar> *data =
+			*(StringInlineData<WideChar> *const *)this;
+		return data ? data->m_text : (const WideChar *)0x0107388C;
+	}
+
+	void concat(const WideChar *text, int length)
+	{
+		StringBase<WideChar>::concat(text, length);
+	}
+};
+
+AsciiString UnicodeStringToQuotedPrintable(UnicodeString original);
+
+__declspec(dllimport) unsigned __cdecl bfmeLenVGI(const WideChar *text);
+
+struct PreferenceNode
+{
+	unsigned char m_pad[0x14];
+	AsciiString m_value;
+};
+
+class PreferenceMap
+{
+public:
+	AsciiString &operator[](const AsciiString &key);
+
+private:
+	PreferenceNode *m_end;
+	unsigned char m_unreconstructed[8];
+};
+
+class UserPreferences : public PreferenceMap
+{
+public:
+	virtual ~UserPreferences();
+	virtual Bool load(AsciiString filename);
+	virtual Bool write(void);
+
+private:
+	AsciiString m_filename;
+};
+
+struct UserNameNode
+{
+	UserNameNode *m_next;
+	UserNameNode *m_prev;
+	UnicodeString m_value;
+};
+
+struct UserNameList
+{
+	UserNameNode *m_end;
+};
+
+class Gen0009FC90Owner : public UserPreferences
+{
+public:
+	void Rva0009FC90(void);
+
+private:
+	UserNameList m_userNames;
+};
+
+// ?Rva0009FC90@Gen0009FC90Owner@@QAEXXZ
+void Gen0009FC90Owner::Rva0009FC90(void)
+{
+	UserNameNode *it = m_userNames.m_end->m_next;
+	UnicodeString names;
+
+	while (it != m_userNames.m_end)
+	{
+		names.concat(it->m_value.str(), it->m_value.getLength());
+
+		const WideChar *comma = (const WideChar *)0x010806D8;
+		names.concat(comma, bfmeLenVGI(comma));
+		it = it->m_next;
+	}
+
+	AsciiString key("UserNames");
+	(*this)[key] = UnicodeStringToQuotedPrintable(names);
+}
