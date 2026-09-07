b8d5bda6b813110b0619b7a9baf9a719610e6e9c
Jonathan <jonathan@users.noreply.github.com>
2026-09-07 16:36:26 -0400

    Recover Peer auto-match room join callback

--- Code/GameEngine/Source/GameNetwork/GameSpy/peer/peerAutoMatchSetStatus.c
+++ Code/GameEngine/Source/GameNetwork/GameSpy/peer/peerAutoMatchSetStatus.c
@@ -5,11 +5,17 @@ typedef int PEERBool;
 
 typedef struct PEERConnection
 {
-	unsigned char pad_0000[0x390];
+	unsigned char pad_0000[4];
+	char nick[64];
+	unsigned char pad_0044[0x390 - 0x44];
 	PEERBool inRoom[3];
-	unsigned char pad_039C[0xB40 - 0x39C];
+	unsigned char pad_039C[0xAB8 - 0x39C];
+	int numPlayers[3];
+	unsigned char pad_0AC4[0xB40 - 0xAC4];
 	PEERBool queryReporting;
-	unsigned char pad_0B44[0x18D4 - 0xB44];
+	unsigned char pad_0B44[0xB48 - 0xB44];
+	int maxPlayers;
+	unsigned char pad_0B4C[0x18D4 - 0xB4C];
 	int autoMatchStatus;
 	unsigned char pad_18D8[0x1EEC - 0x18D8];
 	PEERBool autoMatchBrowsing;
@@ -27,6 +33,7 @@ void piAddAutoMatchStatusCallback(PEER peer);
 void piSBStopListingAutoMatches(PEER peer);
 void piStopAutoMatchReporting(PEER peer);
 void piLeaveRoom(PEER peer, int roomType, const char *reason);
+int piCountRoomOps(PEER peer, int roomType, const char *nick);
 PEERBool piSBStartListingAutoMatches(PEER peer);
 PEERBool piStartAutoMatchReporting(PEER peer);
 extern __declspec(dllimport) void __cdecl gsifree(void *memory);
@@ -114,4 +121,40 @@ void piSetAutoMatchStatus(PEER peer, int status)
 		piCleanAutoMatch(peer);
 		break;
 	}
+}
+
+typedef int PEERJoinResult;
+typedef int RoomType;
+
+void piJoinAutoMatchRoomCallback
+(
+	PEER peer,
+	PEERBool success,
+	PEERJoinResult result,
+	RoomType roomType,
+	void *param
+)
+{
+	if (success && ((peer->numPlayers[2] <= 1) ||
+		!piCountRoomOps(peer, 2, peer->nick)))
+	{
+		piLeaveRoom(peer, 2, "");
+		success = 0;
+	}
+
+	if (success)
+	{
+		piSetAutoMatchStatus(peer, 3);
+
+		if ((peer->autoMatchStatus == 3) &&
+			(peer->numPlayers[2] >= peer->maxPlayers))
+			piSetAutoMatchStatus(peer, 4);
+	}
+
+	if (!success && (peer->autoMatchStatus == 2))
+		piSetAutoMatchStatus(peer, 2);
+
+	(void)result;
+	(void)roomType;
+	(void)param;
 }
