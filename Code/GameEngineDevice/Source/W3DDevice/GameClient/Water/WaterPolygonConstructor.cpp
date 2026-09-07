94be7599a6d017802b289f91d3576e713642568f
dginovker <danielginovker@gmail.com>
2026-09-08 03:56:33 +0900

    Name Water polygon point storage and bounds validity

--- Code/GameEngineDevice/Source/W3DDevice/GameClient/Water/WaterPolygonConstructor.cpp
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/Water/WaterPolygonConstructor.cpp
@@ -34,20 +34,21 @@ public:
 	void initialize(void *source);
 
 private:
-	unsigned char m_firstFlag;
-	unsigned char m_firstPadding[3];
-	void *m_firstValue;
+	unsigned char m_flag04;
+	unsigned char m_padding05[3];
+	void *m_field08;
 	AsciiString m_textureNames[6];
 	RefCountPtr<TextureClass> m_textureReferences[6];
-	unsigned char m_secondFlag;
-	unsigned char m_secondPadding[0x0f];
+	unsigned char m_flag3c;
+	unsigned char m_padding3d[0x0f];
 	unsigned int m_value4c;
 	unsigned int m_value50;
-	unsigned int m_value54;
-	unsigned int m_value58;
+	// The initializer passes these raw values to AABoxClass(Vector3 *, int).
+	unsigned int m_pointStorage;
+	unsigned int m_pointCount;
 	unsigned int m_value5c;
 	unsigned int m_value60;
-	unsigned char m_cached;
+	unsigned char m_boundsValid;
 };
 
 // The initializer is still a generic dump row.  Keep its call target named by
@@ -62,15 +63,15 @@ typedef void (__fastcall *WaterPolygonInitializeCall)(
 	Rva007A1230ArrayOwner *, void *, void *);
 
 Rva007A1230ArrayOwner::Rva007A1230ArrayOwner(void *source)
-	: m_firstFlag(0), m_firstValue(0)
+	: m_flag04(0), m_field08(0)
 {
-	m_secondFlag = 0;
+	m_flag3c = 0;
 	m_value4c = 0;
 	m_value50 = 0;
-	m_value54 = 0;
-	m_value58 = 0;
+	m_pointStorage = 0;
+	m_pointCount = 0;
 	m_value5c = 0;
 	m_value60 = 0;
-	m_cached = 0;
+	m_boundsValid = 0;
 	((WaterPolygonInitializeCall)d_007a4d40)(this, source, source);
 }
