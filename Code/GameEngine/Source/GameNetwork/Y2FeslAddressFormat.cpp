4a491232374db3e4d7752d7980d41f733d612512
dginovker <danielginovker@gmail.com>
2026-09-08 02:55:27 +0900

    Clarify FESL IPv4 formatting parameters and locals

--- Code/GameEngine/Source/GameNetwork/Y2FeslAddressFormat.cpp
+++ Code/GameEngine/Source/GameNetwork/Y2FeslAddressFormat.cpp
@@ -18,8 +18,8 @@ extern "C" int __cdecl sscanf(const char *buf, const char *fmt, ...);
 class Rva007E8760Addr
 {
 public:
-	void format(char *buf, unsigned size);
-	void parse(const char *str, int extra);
+	void format(char *destination, unsigned destinationSize);
+	void parse(const char *addressText, int extra);
 
 private:
 	char m_pad[8];
@@ -27,29 +27,29 @@ private:
 	int m_extra;
 };
 
-void Rva007E8760Addr::format(char *buf, unsigned size)
+void Rva007E8760Addr::format(char *destination, unsigned destinationSize)
 {
-	if (size < 17)
+	if (destinationSize < 17)
 		Rva007EB810Get()->fail(
 			"false",
 			"\\views\\feslbuild_main\\jabba\\fesl\\source\\address.cpp",
 			49);
-	unsigned v = m_addr;
-	sprintf(buf, "%hu.%hu.%hu.%hu",
-		(unsigned char)(v >> 24),
-		(unsigned char)(v >> 16),
-		(unsigned char)(v >> 8),
-		(unsigned char)v);
+	unsigned packedAddress = m_addr;
+	sprintf(destination, "%hu.%hu.%hu.%hu",
+		(unsigned char)(packedAddress >> 24),
+		(unsigned char)(packedAddress >> 16),
+		(unsigned char)(packedAddress >> 8),
+		(unsigned char)packedAddress);
 }
 
-void Rva007E8760Addr::parse(const char *str, int extra)
+void Rva007E8760Addr::parse(const char *addressText, int extra)
 {
-	int a, b, c, d;
-	sscanf(str, "%d.%d.%d.%d", &a, &b, &c, &d);
-	unsigned v = (unsigned)a;
-	v = (v << 8) + (unsigned)b;
-	v = (v << 8) + (unsigned)c;
-	v = (v << 8) + (unsigned)d;
-	m_addr = v;
+	int firstOctet, secondOctet, thirdOctet, fourthOctet;
+	sscanf(addressText, "%d.%d.%d.%d", &firstOctet, &secondOctet, &thirdOctet, &fourthOctet);
+	unsigned packedAddress = (unsigned)firstOctet;
+	packedAddress = (packedAddress << 8) + (unsigned)secondOctet;
+	packedAddress = (packedAddress << 8) + (unsigned)thirdOctet;
+	packedAddress = (packedAddress << 8) + (unsigned)fourthOctet;
+	m_addr = packedAddress;
 	m_extra = extra;
 }
