30bcabee830402e66c9af6ae9bc6d7ceeb92caab
TheThag <zackgreco@hotmail.com>
2026-09-07 05:20:11 -0300

    Luna36h: convert QuickMatchPreferences getColor

--- Code/GameEngine/Source/Common/UserPreferences.cpp
+++ Code/GameEngine/Source/Common/UserPreferences.cpp
@@ -521,43 +521,9 @@ void QuickMatchPreferences::setColor( Int val )
 	setInt("Color", val);
 }
 
-__declspec(naked) Int QuickMatchPreferences::getColor( void )
+Int QuickMatchPreferences::getColor( void )
 {
-	__asm {
-		_emit 051h
-		_emit 056h
-		_emit 06Ah
-		_emit 000h
-		_emit 051h
-		_emit 08Bh
-		_emit 0F1h
-		_emit 089h
-		_emit 064h
-		_emit 024h
-		_emit 00Ch
-		_emit 08Bh
-		_emit 0CCh
-		_emit 068h
-		_emit 050h
-		_emit 0C7h
-		_emit 007h
-		_emit 001h
-		_emit 0E8h
-		_emit 0E9h
-		_emit 0F3h
-		_emit 07Dh
-		_emit 000h
-		_emit 08Bh
-		_emit 0CEh
-		_emit 0E8h
-		_emit 0DBh
-		_emit 0ABh
-		_emit 0F9h
-		_emit 0FFh
-		_emit 05Eh
-		_emit 059h
-		_emit 0C3h
-	}
+	return getInt( "Color", 0 );
 }
 
 void QuickMatchPreferences::setSide( Int val )
