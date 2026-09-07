119cc727c787e8e3b3f835a803139fdd6070f19c
dginovker <danielginovker@gmail.com>
2026-09-08 03:27:17 +0900

    Place ArchivedFileInfo pair copies at their actual retail bodies

---
+++ Code/GameEngine/Source/Common/System/ArchivedFileInfoPairConstruct.cpp
@@ -0,0 +1,40 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB
+// stlport
+
+#include <utility>
+#include <memory>
+
+class AsciiString;
+
+template <typename T>
+class StringBase
+{
+    friend class AsciiString;
+private:
+    StringBase(const StringBase<T> &src);
+    void *m_data;
+};
+
+// Retail's pair copy inlines this forwarder and calls the narrow StringBase body.
+class AsciiString : private StringBase<char>
+{
+public:
+    __forceinline AsciiString(const AsciiString &src) : StringBase<char>(src) {}
+    ~AsciiString();
+};
+
+// The matched copy at 0x009CEBF0 copies strings at +0/+4 and uints at +8/+0xC.
+class ArchivedFileInfo
+{
+public:
+    ArchivedFileInfo(const ArchivedFileInfo &);
+    AsciiString m_filename;
+    AsciiString m_archiveFilename;
+    unsigned int m_offset;
+    unsigned int m_size;
+};
+
+typedef std::pair<const AsciiString, ArchivedFileInfo> ArchivedFileInfoPair;
+
+// Keep the pair copy out of the helper, as retail 0x009CF3C0 calls 0x009CF180.
+template void _STL::_Construct(ArchivedFileInfoPair *, const ArchivedFileInfoPair &);
