29064a1a61a1d01fee6d0c3f327adc0c82efd3da
Gabe Borges <gbborges@proton.me>
2026-09-07 09:35:36 -0400

    feat(common): convert bfmeReadSJA
    The bfmeGoSJA caller and the 0x0000FB73 incremental-link thunk identify retail RVA 0x00462420 as bfmeReadSJA, and the body calls the named AsciiString hash erase specialization at 0x00460A70. The clean map walk lives in Code/GameEngine/Source/Common/BfmeConv1292.cpp beside its caller and the paired bfmeLoadSJA declaration.

--- Code/GameEngine/Source/Common/BfmeConv1292.cpp
+++ Code/GameEngine/Source/Common/BfmeConv1292.cpp
@@ -1,10 +1,65 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/stringinline
+// stlport
 // Open-BFME5 conversions.
+
+#define _STLP_NO_EXCEPTIONS 1
+#include <hash_map>
+
+#include "StringInline.h"
+
+class Rva00460A70Mapped
+{
+public:
+	virtual void release( int deleting ) = 0;
+
+	void *m_owner;
+};
+
+namespace rts
+{
+	template <class T> struct hash;
+	template <class T> struct equal_to;
+
+	template <> struct hash<AsciiString>
+	{
+		unsigned int operator()( AsciiString value ) const;
+	};
+
+	template <> struct equal_to<AsciiString>
+	{
+		int operator()( const AsciiString &left, const AsciiString &right ) const;
+	};
+}
+
+typedef _STL::hash_map<AsciiString, Rva00460A70Mapped *, rts::hash<AsciiString>,
+	_STL::equal_to<AsciiString> > BfmeSJAHash;
 
 extern char g_bfmeOneSJA[];
 extern char g_bfmeTwoSJA[];
 extern char g_bfmeDoneSJA;
 
-void bfmeReadSJA(void *slot, void *p, char *out);
+void bfmeReadSJA(void *slot, void *p, char *out)
+{
+	BfmeSJAHash *table = (BfmeSJAHash *)slot;
+	BfmeSJAHash::iterator eraseIt;
+	BfmeSJAHash::iterator it = table->begin();
+	while (it != table->end())
+	{
+		if (it->second->m_owner == p)
+		{
+			eraseIt = it++;
+			BfmeSJAHash::value_type *entry = &*eraseIt;
+			Rva00460A70Mapped *mapped = entry->second;
+			if (mapped)
+				mapped->release( 1 );
+			table->erase( eraseIt );
+		}
+		else
+		{
+			++it;
+		}
+	}
+}
 void bfmeLoadSJA(void *slot, void *p, char *out);
 
 void bfmeGoSJA(void *p)
