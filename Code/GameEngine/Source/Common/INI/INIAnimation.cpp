bd482bc71cdb35b46193c02f5b1961de5c3b446d
dginovker <danielginovker@gmail.com>
2026-09-08 10:29:14 +0900

    Consolidate INI animation parsers

--- Code/GameEngine/Source/Common/INI/INIAnimation.cpp
+++ Code/GameEngine/Source/Common/INI/INIAnimation.cpp
@@ -1,10 +1,7 @@
 // cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
-// Open-BFME: INI::parseAnim2DDefinition, retail 0x005BB400.
-
 #include <string.h>
 
 typedef int Int;
-
 struct FieldParse;
 
 template <typename T>
@@ -17,23 +14,30 @@ public:
 
 private:
 	StringBase(const T *text);
-	void releaseBuffer();
-
+	~StringBase();
 	void *m_data;
-
 	friend class AsciiString;
 };
 
-class AsciiString
+class AsciiString : private StringBase<char>
 {
 public:
-	AsciiString() : m_string() {}
-	~AsciiString() { m_string.releaseBuffer(); }
+	AsciiString() : StringBase<char>() {}
+	AsciiString(const char *text) : StringBase<char>(text) {}
+	void set(const char *text) { StringBase<char>::set(text); }
+};
 
-	void set(const char *text) { m_string.set(text); }
+// BFME adds an integer after the message pointer; its meaning is unresolved.
+class INIException
+{
+public:
+	INIException(Int, const char *, ...);
+	INIException(const INIException &);
+	~INIException();
 
 private:
-	StringBase<char> m_string;
+	char *m_message;
+	Int m_unreconstructed04;
 };
 
 class Anim2DTemplate
@@ -56,10 +60,11 @@ class INI
 public:
 	const char *getNextToken(const char *seps = 0);
 	void initFromINI(void *what, const FieldParse *parseTable);
-
 	static void parseAnim2DDefinition(INI *ini);
+	static void parseAnim2DTemplate(INI *ini, void *instance, void *store, const void *userData);
 };
 
+// ?parseAnim2DDefinition@INI@@SAXPAV1@@Z
 void INI::parseAnim2DDefinition( INI *ini )
 {
 	AsciiString name;
@@ -76,5 +81,21 @@ void INI::parseAnim2DDefinition( INI *ini )
 	{
 		animTemplate = TheAnim2DCollection->newTemplate( name );
 		ini->initFromINI( animTemplate, Anim2DTemplate::s_anim2DFieldParseTable );
+	}
+}
+
+// ?parseAnim2DTemplate@INI@@SAXPAV1@PAX1PBX@Z
+void INI::parseAnim2DTemplate(INI *ini, void *instance, void *store, const void *userData)
+{
+	const char *token = ini->getNextToken();
+
+	if (TheAnim2DCollection)
+	{
+		Anim2DTemplate **anim2DTemplate = (Anim2DTemplate **)store;
+		*anim2DTemplate = TheAnim2DCollection->findTemplate(AsciiString(token));
+	}
+	else
+	{
+		throw INIException(9, "iniParseAnim2DTemplate - TheAnim2DCollection is NULL");
 	}
 }
