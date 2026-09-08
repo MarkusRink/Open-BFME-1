cde17cba46b8f65a3809dbcda511b3efadb27a52
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 06:16:03 -0400

    Recover BFME registry display name getter

--- Code/GameEngine/Source/Common/Upgrade_loadPostProcessMethodThunk.cpp
+++ Code/GameEngine/Source/Common/Upgrade_loadPostProcessMethodThunk.cpp
@@ -1,189 +1,39 @@
 // cl: /DNDEBUG /MD /EHsc
-// readable body of ?loadPostProcess@Upgrade@@MAEXXZ: Code/GameEngine/Source/Common/System/Upgrade.cpp
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Retail 0x00109560 is a global Unicode registry display-name getter.  It was
+// previously misidentified as Upgrade::loadPostProcess, whose reference body
+// is empty and whose this pointer the retail function never reads.
 
-class __declspec(novtable) Upgrade
+template <typename T>
+class StringBase
 {
 protected:
-    virtual void loadPostProcess();
+	StringBase(const T *text);
+	StringBase(const StringBase<T> &that);
+	~StringBase();
+
+private:
+	void *m_data;
 };
 
-// ?loadPostProcess@Upgrade@@MAEXXZ
-__declspec(naked) void Upgrade::loadPostProcess()
+class UnicodeString : private StringBase<unsigned short>
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x69
-        __emit 0xd0
-        __emit 0xff
-        __emit 0x00
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xec
-        __emit 0x10
-        __emit 0x56
-        __emit 0x68
-        __emit 0xb4
-        __emit 0x89
-        __emit 0x08
-        __emit 0x01
-        __emit 0x8d
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x51
-        __emit 0xf8
-        __emit 0x77
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x44
-        __emit 0x24
-        __emit 0x04
-        __emit 0x50
-        __emit 0x51
-        __emit 0x89
-        __emit 0x64
-        __emit 0x24
-        __emit 0x14
-        __emit 0x8b
-        __emit 0xcc
-        __emit 0x68
-        __emit 0x98
-        __emit 0x89
-        __emit 0x08
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x28
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x33
-        __emit 0xf8
-        __emit 0x77
-        __emit 0x00
-        __emit 0x51
-        __emit 0x89
-        __emit 0x64
-        __emit 0x24
-        __emit 0x1c
-        __emit 0x8b
-        __emit 0xcc
-        __emit 0x68
-        __emit 0x28
-        __emit 0x89
-        __emit 0x08
-        __emit 0x01
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x2c
-        __emit 0x02
-        __emit 0xe8
-        __emit 0x1d
-        __emit 0xf8
-        __emit 0x77
-        __emit 0x00
-        __emit 0x68
-        __emit 0x02
-        __emit 0x00
-        __emit 0x00
-        __emit 0x80
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x2c
-        __emit 0x01
-        __emit 0xe8
-        __emit 0x73
-        __emit 0xbd
-        __emit 0xf3
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x74
-        __emit 0x24
-        __emit 0x34
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0x8d
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x04
-        __emit 0x51
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xe8
-        __emit 0x1b
-        __emit 0xee
-        __emit 0x77
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x04
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x08
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x1c
-        __emit 0x00
-        __emit 0xe8
-        __emit 0xd5
-        __emit 0xeb
-        __emit 0x77
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x14
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x5e
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x1c
-        __emit 0xc3
-    }
+public:
+	UnicodeString(const unsigned short *text) : StringBase<unsigned short>(text) {}
+	UnicodeString(const UnicodeString &that) : StringBase<unsigned short>(that) {}
+	~UnicodeString() {}
+};
+
+bool getStringFromRegistry(
+	void *root, UnicodeString path, UnicodeString key, UnicodeString &value);
+
+// ?getBfmeDisplayName@@YA?AVUnicodeString@@XZ
+UnicodeString getBfmeDisplayName()
+{
+	UnicodeString value(L"The Battle for Middle-earth");
+	getStringFromRegistry(
+		reinterpret_cast<void *>(0x80000002),
+		UnicodeString(L"SOFTWARE\\EA Games\\The Battle for Middle-earth"),
+		UnicodeString(L"DisplayName"),
+		value);
+	return value;
 }
