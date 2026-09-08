794c782816d7da454e3ac8226ee6e2d676332901
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 17:12:28 -0400

    game: convert BroadcastStealthUpdate constructor

--- Code/GameEngine/Source/GameLogic/Object/Update/BroadcastStealthUpdateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/BroadcastStealthUpdateCtorThunk.cpp
@@ -1,261 +1,74 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB
+// stlport
+
+// Open-BFME5: BroadcastStealthUpdate constructor.  Its module factory and
+// destructor establish the class while the two STLport lists fix the tail.
+
+#include <list>
 
 class Thing;
 class ModuleData;
+class Object;
 
-class BroadcastStealthUpdate
+class ObjectModule
 {
 public:
-    BroadcastStealthUpdate(Thing *, const ModuleData *);
+	ObjectModule(Thing *, const ModuleData *);
+	virtual ~ObjectModule();
+protected:
+	const ModuleData *m_moduleData;
+	Object *m_object;
+};
+class BehaviorModuleInterface { public: virtual void slot(); };
+class LargeGroupBonusIface2 { public: virtual void slot(); };
+
+class BehaviorModule : public ObjectModule,
+	public BehaviorModuleInterface, public LargeGroupBonusIface2
+{
+public:
+	BehaviorModule(Thing *thing, const ModuleData *data)
+		: ObjectModule(thing, data), m_value14(0), m_value18(-1), m_value1c(-1) {}
+	virtual ~BehaviorModule();
+private:
+	unsigned int m_value14;
+	int m_value18;
+	int m_value1c;
+};
+
+class SpyVisionUpgradeMux
+{
+public:
+	SpyVisionUpgradeMux();
+	virtual void slot();
+private:
+	bool m_upgradeExecuted;
+	unsigned char m_pad[3];
+};
+
+class UpdateModule
+{
+public:
+	void setWakeFrame(Object *, unsigned int);
+};
+
+class BroadcastStealthUpdate : public BehaviorModule,
+	public SpyVisionUpgradeMux
+{
+public:
+	BroadcastStealthUpdate(Thing *, const ModuleData *);
+	virtual ~BroadcastStealthUpdate();
+	virtual void slot();
+private:
+	_STL::list<int> m_broadcastObjects;
+	unsigned int m_broadcastObjectCount;
+	_STL::list<int> m_pendingObjects;
 };
 
 // ??0BroadcastStealthUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) BroadcastStealthUpdate::BroadcastStealthUpdate(Thing *, const ModuleData *)
+BroadcastStealthUpdate::BroadcastStealthUpdate(
+	Thing *thing, const ModuleData *data)
+	: BehaviorModule(thing, data), SpyVisionUpgradeMux()
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x6e;
-        __emit 0x11;
-        __emit 0x01;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x53;
-        __emit 0x56;
-        __emit 0x57;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x20;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0xe8;
-        __emit 0x34;
-        __emit 0xd8;
-        __emit 0xd8;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xc9;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xa0;
-        __emit 0xcb;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0x83;
-        __emit 0xc8;
-        __emit 0xff;
-        __emit 0x33;
-        __emit 0xdb;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x94;
-        __emit 0xcd;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xcc;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xac;
-        __emit 0xcb;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x14;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x1c;
-        __emit 0x8d;
-        __emit 0x7e;
-        __emit 0x20;
-        __emit 0x8b;
-        __emit 0xcf;
-        __emit 0x89;
-        __emit 0x5c;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0xe8;
-        __emit 0x60;
-        __emit 0x39;
-        __emit 0xdb;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x0c;
-        __emit 0xcc;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0x48;
-        __emit 0xcb;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0x3c;
-        __emit 0xcb;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x07;
-        __emit 0xf0;
-        __emit 0xca;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0x6a;
-        __emit 0x0c;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x28;
-        __emit 0xe8;
-        __emit 0x2e;
-        __emit 0x4c;
-        __emit 0x5a;
-        __emit 0x00;
-        __emit 0x89;
-        __emit 0x00;
-        __emit 0x89;
-        __emit 0x40;
-        __emit 0x04;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x28;
-        __emit 0x6a;
-        __emit 0x0c;
-        __emit 0xc6;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x20;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x30;
-        __emit 0xe8;
-        __emit 0x17;
-        __emit 0x4c;
-        __emit 0x5a;
-        __emit 0x00;
-        __emit 0x89;
-        __emit 0x00;
-        __emit 0x89;
-        __emit 0x40;
-        __emit 0x04;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x08;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x30;
-        __emit 0x8b;
-        __emit 0x56;
-        __emit 0x08;
-        __emit 0x68;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0x3f;
-        __emit 0x52;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xc6;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x20;
-        __emit 0x02;
-        __emit 0xe8;
-        __emit 0x91;
-        __emit 0xbe;
-        __emit 0xd8;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4e;
-        __emit 0x28;
-        __emit 0x8b;
-        __emit 0x01;
-        __emit 0x33;
-        __emit 0xd2;
-        __emit 0x3b;
-        __emit 0xc1;
-        __emit 0x74;
-        __emit 0x07;
-        __emit 0x8b;
-        __emit 0x00;
-        __emit 0x42;
-        __emit 0x3b;
-        __emit 0xc1;
-        __emit 0x75;
-        __emit 0xf9;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x10;
-        __emit 0x5f;
-        __emit 0x89;
-        __emit 0x56;
-        __emit 0x2c;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x5b;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+	((UpdateModule *)this)->setWakeFrame(m_object, 0x3fffffff);
+	m_broadcastObjectCount = m_broadcastObjects.size();
 }
