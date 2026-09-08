59d3951dc9d4c9446b1850d95f04ccba7c434865
Jonathan <jonathan@users.noreply.github.com>
2026-09-07 23:33:29 -0400

    Recover Peer auto-match room creation callback

--- Code/GameEngine/Source/GameNetwork/GameSpy/peer/peerAutoMatchCreateRoom.c
+++ Code/GameEngine/Source/GameNetwork/GameSpy/peer/peerAutoMatchCreateRoom.c
@@ -14,21 +14,61 @@ typedef struct PEERConnection
 {
 	unsigned char pad_0000[4];
 	char nick[64];
-	unsigned char pad_0044[0xB48 - 0x44];
+	unsigned char pad_0044[0xAB8 - 0x44];
+	int numPlayers[3];
+	unsigned char pad_0AC4[0xB48 - 0xAC4];
 	int maxPlayers;
 	unsigned char pad_0B4C[0x1EF0 - 0xB4C];
 	piOperation *autoMatchOperation;
-	unsigned char pad_1EF4[0x1F00 - 0x1EF4];
+	unsigned char pad_1EF4[0x1EFC - 0x1EF4];
+	PEERBool autoMatchSBFailed;
 	PEERBool autoMatchQRFailed;
 } PEERConnection;
 
 typedef PEERConnection *PEER;
 
 int piGetNextID(PEER peer);
-void piCreateAutoMatchRoomCallback(void);
+void piSetAutoMatchStatus(PEER peer, int status);
 int piNewCreateStagingRoomOperation(PEER peer, const char *nick,
 	const char *password, int maxPlayers, int socket, unsigned short port,
 	void *callback, void *param, int opID);
+
+typedef int PEERJoinResult;
+typedef int RoomType;
+
+void piCreateAutoMatchRoomCallback
+(
+	PEER peer,
+	PEERBool success,
+	PEERJoinResult result,
+	RoomType roomType,
+	void *param
+)
+{
+	int status;
+
+	if (success)
+	{
+		if (peer->numPlayers[2] <= 1)
+			status = 2;
+		else if (peer->numPlayers[2] >= peer->maxPlayers)
+			status = 4;
+		else
+			status = 3;
+
+		piSetAutoMatchStatus(peer, status);
+	}
+	else
+	{
+		peer->autoMatchQRFailed = 1;
+		piSetAutoMatchStatus(peer,
+			peer->autoMatchSBFailed ? 0 : 1);
+	}
+
+	(void)result;
+	(void)roomType;
+	(void)param;
+}
 
 PEERBool piCreateAutoMatchRoom(PEER peer)
 {
