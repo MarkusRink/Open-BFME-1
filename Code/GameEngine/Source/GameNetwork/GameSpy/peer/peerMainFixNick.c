0f1982660d87ddd1a09bd94ecc825d9535107509
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 03:33:21 -0400

    Convert GameSpy peer nickname repair wrapper to C

---
+++ Code/GameEngine/Source/GameNetwork/GameSpy/peer/peerMainFixNick.c
@@ -0,0 +1,9 @@
+// cl: /DNDEBUG /MD
+// Upstream: GameSpy Peer SDK peerMain.c, 2007 release.
+
+void Rva00860620(char *newNick, const char *oldNick);
+
+void peerFixNickA(char *newNick, const char *oldNick)
+{
+	Rva00860620(newNick, oldNick);
+}
