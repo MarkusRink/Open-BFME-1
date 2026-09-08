6318a406fb91f3c516ccdf82bd35845de39af049
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 16:13:37 -0400

    game: convert AODHordeContain constructor

--- Code/GameEngine/Source/GameLogic/AI/AODHordeContainCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/AODHordeContainCtorThunk.cpp
@@ -1,342 +1,118 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB
+// stlport
+
+// Open-BFME5: AODHordeContain constructor.  Factory/destructor evidence fixes
+// the HordeContain base, vector, and both compiler-managed element arrays.
+
+#include <vector>
 
 class Thing;
 class ModuleData;
 
-class AODHordeContain
+class OpenContainPrimaryBase { public: virtual ~OpenContainPrimaryBase() {} private: unsigned char m_pad[8]; };
+template <int Number> class OpenContainSecondaryBase { public: virtual ~OpenContainSecondaryBase() {} };
+class OpenContainWideSecondaryBase { public: virtual ~OpenContainWideSecondaryBase() {} private: unsigned char m_pad[12]; };
+
+class __declspec(novtable) OpenContain
+	: public OpenContainPrimaryBase, public OpenContainSecondaryBase<1>,
+	  public OpenContainWideSecondaryBase, public OpenContainSecondaryBase<2>,
+	  public OpenContainSecondaryBase<3>, public OpenContainSecondaryBase<4>,
+	  public OpenContainSecondaryBase<5>, public OpenContainSecondaryBase<6>,
+	  public OpenContainSecondaryBase<7>
 {
-public:
-    AODHordeContain(Thing *, const ModuleData *);
+public: virtual ~OpenContain() {}
+private: unsigned char m_pad[0x9c];
 };
 
-__declspec(naked) AODHordeContain::AODHordeContain(Thing *, const ModuleData *)
+class SiegeEngineContainTenthBase { public: virtual ~SiegeEngineContainTenthBase() {} };
+class HordeContainEleventhBase { public: virtual void slot(); };
+
+class __declspec(novtable) SiegeEngineContainBase
+	: public OpenContain, public SiegeEngineContainTenthBase
 {
-    __asm {
-        _emit 06Ah
-        _emit 0FFh
-        _emit 068h
-        _emit 09Eh
-        _emit 0D8h
-        _emit 000h
-        _emit 001h
-        _emit 064h
-        _emit 0A1h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 050h
-        _emit 064h
-        _emit 089h
-        _emit 025h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 051h
-        _emit 08Bh
-        _emit 044h
-        _emit 024h
-        _emit 018h
-        _emit 056h
-        _emit 057h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 08Bh
-        _emit 04Ch
-        _emit 024h
-        _emit 01Ch
-        _emit 050h
-        _emit 051h
-        _emit 08Bh
-        _emit 0CEh
-        _emit 089h
-        _emit 074h
-        _emit 024h
-        _emit 010h
-        _emit 0E8h
-        _emit 050h
-        _emit 0A3h
-        _emit 0E0h
-        _emit 0FFh
-        _emit 033h
-        _emit 0FFh
-        _emit 0C7h
-        _emit 006h
-        _emit 0A0h
-        _emit 0E7h
-        _emit 00Ah
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 00Ch
-        _emit 0D8h
-        _emit 0E6h
-        _emit 00Ah
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 010h
-        _emit 0C8h
-        _emit 0E6h
-        _emit 00Ah
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 020h
-        _emit 020h
-        _emit 0E5h
-        _emit 00Ah
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 024h
-        _emit 000h
-        _emit 0E5h
-        _emit 00Ah
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 028h
-        _emit 0FCh
-        _emit 0E4h
-        _emit 00Ah
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 02Ch
-        _emit 0ECh
-        _emit 0E4h
-        _emit 00Ah
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 030h
-        _emit 0B0h
-        _emit 0E4h
-        _emit 00Ah
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 034h
-        _emit 0A0h
-        _emit 0E4h
-        _emit 00Ah
-        _emit 001h
-        _emit 0C7h
-        _emit 086h
-        _emit 0D4h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 09Ch
-        _emit 0E4h
-        _emit 00Ah
-        _emit 001h
-        _emit 0C7h
-        _emit 086h
-        _emit 0E4h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 030h
-        _emit 0E2h
-        _emit 00Ah
-        _emit 001h
-        _emit 089h
-        _emit 0BEh
-        _emit 024h
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 089h
-        _emit 0BEh
-        _emit 028h
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 089h
-        _emit 07Ch
-        _emit 024h
-        _emit 014h
-        _emit 089h
-        _emit 0BEh
-        _emit 02Ch
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 068h
-        _emit 010h
-        _emit 0FEh
-        _emit 041h
-        _emit 000h
-        _emit 068h
-        _emit 0C9h
-        _emit 02Bh
-        _emit 042h
-        _emit 000h
-        _emit 06Ah
-        _emit 03Ch
-        _emit 089h
-        _emit 0BEh
-        _emit 030h
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 089h
-        _emit 0BEh
-        _emit 034h
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 089h
-        _emit 0BEh
-        _emit 038h
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 089h
-        _emit 0BEh
-        _emit 03Ch
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 089h
-        _emit 0BEh
-        _emit 040h
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 06Ah
-        _emit 010h
-        _emit 08Dh
-        _emit 096h
-        _emit 050h
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 052h
-        _emit 0C6h
-        _emit 044h
-        _emit 024h
-        _emit 028h
-        _emit 001h
-        _emit 089h
-        _emit 0BEh
-        _emit 044h
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 089h
-        _emit 0BEh
-        _emit 048h
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 089h
-        _emit 0BEh
-        _emit 04Ch
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 0E8h
-        _emit 07Ch
-        _emit 068h
-        _emit 07Ch
-        _emit 000h
-        _emit 068h
-        _emit 01Dh
-        _emit 01Ah
-        _emit 041h
-        _emit 000h
-        _emit 068h
-        _emit 0F1h
-        _emit 04Dh
-        _emit 043h
-        _emit 000h
-        _emit 06Ah
-        _emit 014h
-        _emit 06Ah
-        _emit 018h
-        _emit 08Dh
-        _emit 086h
-        _emit 014h
-        _emit 006h
-        _emit 000h
-        _emit 000h
-        _emit 050h
-        _emit 0C6h
-        _emit 044h
-        _emit 024h
-        _emit 028h
-        _emit 002h
-        _emit 089h
-        _emit 0BEh
-        _emit 010h
-        _emit 006h
-        _emit 000h
-        _emit 000h
-        _emit 0E8h
-        _emit 057h
-        _emit 068h
-        _emit 07Ch
-        _emit 000h
-        _emit 089h
-        _emit 0BEh
-        _emit 0F4h
-        _emit 007h
-        _emit 000h
-        _emit 000h
-        _emit 08Dh
-        _emit 086h
-        _emit 058h
-        _emit 002h
-        _emit 000h
-        _emit 000h
-        _emit 0B9h
-        _emit 03Ch
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 08Bh
-        _emit 0FFh
-        _emit 089h
-        _emit 078h
-        _emit 0F8h
-        _emit 089h
-        _emit 078h
-        _emit 0FCh
-        _emit 089h
-        _emit 038h
-        _emit 089h
-        _emit 078h
-        _emit 004h
-        _emit 083h
-        _emit 0C0h
-        _emit 010h
-        _emit 049h
-        _emit 075h
-        _emit 0EFh
-        _emit 08Bh
-        _emit 04Ch
-        _emit 024h
-        _emit 00Ch
-        _emit 05Fh
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 064h
-        _emit 089h
-        _emit 00Dh
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 083h
-        _emit 0C4h
-        _emit 010h
-        _emit 0C2h
-        _emit 008h
-        _emit 000h
-    }
+public: virtual ~SiegeEngineContainBase();
+private: unsigned char m_pad[0x0c];
+};
+
+class __declspec(novtable) HorseHordeContainBase
+	: public SiegeEngineContainBase, public HordeContainEleventhBase
+{
+public:
+	HorseHordeContainBase(Thing *, const ModuleData *);
+	virtual ~HorseHordeContainBase();
+private:
+	unsigned char m_pad[0x13c];
+};
+
+struct Gen_p24pod { int a[6]; };
+
+class AODHordeContainFields230
+{
+public:
+	AODHordeContainFields230()
+	{
+		m_values[0] = 0;
+		m_values[1] = 0;
+		m_values[2] = 0;
+		m_values[3] = 0;
+		m_values[4] = 0;
+	}
+private:
+	unsigned int m_values[5];
+};
+
+class AODHordeContainFields244
+{
+public:
+	AODHordeContainFields244()
+	{
+		m_values[0] = 0;
+		m_values[1] = 0;
+		m_values[2] = 0;
+	}
+private:
+	unsigned int m_values[3];
+};
+
+class AODHordeContainElement16
+{
+public:
+	AODHordeContainElement16();
+	~AODHordeContainElement16();
+	void clear() { m_words[0] = 0; m_words[1] = 0; m_words[2] = 0; m_words[3] = 0; }
+private:
+	unsigned int m_words[4];
+};
+
+class AODHordeContainElement24
+{
+public:
+	AODHordeContainElement24();
+	~AODHordeContainElement24();
+private:
+	unsigned char m_bytes[0x18];
+};
+
+class AODHordeContain : public HorseHordeContainBase
+{
+public:
+	AODHordeContain(Thing *, const ModuleData *);
+	virtual ~AODHordeContain();
+private:
+	_STL::vector<Gen_p24pod> m_vector;
+	AODHordeContainFields230 m_fields230;
+	AODHordeContainFields244 m_fields244;
+	AODHordeContainElement16 m_arrayA[0x3c];
+	unsigned int m_610;
+	AODHordeContainElement24 m_arrayB[0x14];
+	unsigned int m_7f4;
+};
+
+// ??0AODHordeContain@@QAE@PAVThing@@PBVModuleData@@@Z
+AODHordeContain::AODHordeContain(Thing *thing, const ModuleData *data)
+	: HorseHordeContainBase(thing, data), m_610(0)
+{
+	m_7f4 = 0;
+	for (int j = 0; j != 0x3c; ++j)
+		m_arrayA[j].clear();
 }
