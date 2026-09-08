82fb12a66dc8ca59b8419cbcbd43b8c66fd1b42a
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 07:40:57 -0400

    Convert default draw template factory to C++

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/ConcreteModuleClassDefaultModuleTag5CreateTemplateThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/ConcreteModuleClassDefaultModuleTag5CreateTemplateThunk.cpp
@@ -1,124 +1,49 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHsc /O2 /Ob2
 
 namespace FXParticleSystem
 {
-template<int Category>
-class DefaultModuleTag
+
+template<int Category> class DefaultModuleTag {};
+template<int Category> class DefaultModuleTemplate {};
+
+class DefaultModuleTemplate6CtorShim
 {
+public:
+	void construct();
 };
 
-template<int Category>
-class DefaultModuleTemplate
+extern "C" char DefaultModuleTemplate6Concrete_vtbl0;
+extern "C" char DefaultModuleTemplate6Concrete_vtbl4;
+extern "C" char DefaultModuleTemplate6Concrete_vtbl8;
+
+class DefaultModuleTemplate6Allocation
 {
+public:
+	DefaultModuleTemplate6Allocation()
+	{
+		((DefaultModuleTemplate6CtorShim *)this)->construct();
+		*(void **)((char *)this + 0) = &DefaultModuleTemplate6Concrete_vtbl0;
+		*(void **)((char *)this + 4) = &DefaultModuleTemplate6Concrete_vtbl4;
+		*(void **)((char *)this + 8) = &DefaultModuleTemplate6Concrete_vtbl8;
+	}
+
+private:
+	unsigned char bytes_[0x0C];
 };
 
 template<class Tag>
 class ConcreteModuleClass
 {
 public:
-    virtual DefaultModuleTemplate<6> *createTemplate() const;
+	virtual DefaultModuleTemplate<6> *createTemplate() const;
 };
 
 template<class Tag>
-__declspec(naked) DefaultModuleTemplate<6> *ConcreteModuleClass<Tag>::createTemplate() const
+DefaultModuleTemplate<6> *ConcreteModuleClass<Tag>::createTemplate() const
 {
-    __asm {
-        _emit 06Ah
-        _emit 0FFh
-        _emit 068h
-        _emit 03Bh
-        _emit 0B6h
-        _emit 003h
-        _emit 001h
-        _emit 064h
-        _emit 0A1h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 050h
-        _emit 064h
-        _emit 089h
-        _emit 025h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 051h
-        _emit 056h
-        _emit 06Ah
-        _emit 00Ch
-        _emit 0E8h
-        _emit 0E2h
-        _emit 0E0h
-        _emit 029h
-        _emit 000h
-        _emit 08Bh
-        _emit 0F0h
-        _emit 083h
-        _emit 0C4h
-        _emit 004h
-        _emit 089h
-        _emit 074h
-        _emit 024h
-        _emit 004h
-        _emit 033h
-        _emit 0C0h
-        _emit 03Bh
-        _emit 0F0h
-        _emit 089h
-        _emit 044h
-        _emit 024h
-        _emit 010h
-        _emit 074h
-        _emit 01Dh
-        _emit 08Bh
-        _emit 0CEh
-        _emit 0E8h
-        _emit 0C2h
-        _emit 0BDh
-        _emit 0A4h
-        _emit 0FFh
-        _emit 0C7h
-        _emit 006h
-        _emit 00Ch
-        _emit 00Eh
-        _emit 011h
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 004h
-        _emit 008h
-        _emit 00Eh
-        _emit 011h
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 008h
-        _emit 0F4h
-        _emit 00Dh
-        _emit 011h
-        _emit 001h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 08Bh
-        _emit 04Ch
-        _emit 024h
-        _emit 008h
-        _emit 05Eh
-        _emit 064h
-        _emit 089h
-        _emit 00Dh
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 083h
-        _emit 0C4h
-        _emit 010h
-        _emit 0C3h
-    }
+	return (DefaultModuleTemplate<6> *)new DefaultModuleTemplate6Allocation;
 }
 
 template class ConcreteModuleClass<DefaultModuleTag<6> >;
+
 }
