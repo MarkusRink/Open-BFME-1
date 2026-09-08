580395684256bf4ecc3bc687f1e1cbdff6d46e8a
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 02:40:17 -0400

    wwmath: replace naked CIasin import stub

--- Code/Libraries/Source/WWVegas/WWMath/CIasinThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWMath/CIasinThunk.cpp
@@ -1,11 +1,6 @@
-extern "C" __declspec(naked) void __CIasin()
+extern "C" __declspec(dllimport) void bfmeCIasinImport();
+
+extern "C" void __CIasin()
 {
-    __asm {
-        _emit 0FFh
-        _emit 025h
-        _emit 034h
-        _emit 092h
-        _emit 035h
-        _emit 001h
-    }
+	bfmeCIasinImport();
 }
