0e60786dc78bcb9b2ed2022447540472b275434d
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 10:28:02 -0400

    Convert AI idle state constructor to C++

--- Code/GameEngine/Source/GameLogic/AI/AIIdleStateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/AIIdleStateCtorThunk.cpp
@@ -1,91 +1,52 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHsc /Ireference/shims/sweep /Ireference/shims/campaignmanagerascii /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /ICode/Libraries/Source/WWVegas/WWLib
+
+#include "Common/AsciiString.h"
 
 class StateMachine;
 
-// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/AIStateMachine.h
-class AIIdleState
+// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/Common/StateMachine.h
+class State
 {
 public:
-    enum AIIdleTargetingType
-    {
-    };
+	State(StateMachine *machine, AsciiString name);
 
-    AIIdleState(StateMachine *, AIIdleTargetingType);
+protected:
+	int *volatile m_vftable;
+	unsigned char m_head[0x20];
 };
 
-__declspec(naked) AIIdleState::AIIdleState(StateMachine *, AIIdleTargetingType)
+extern int g_AIIdleStateVTable;
+
+// upstream source and layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/AIStateMachine.h
+// BFME retains the targeting mode and one cleared word after the three fields
+// shared with the later Generals version; the retail constructor proves both.
+class AIIdleState : public State
 {
-    __asm {
-        _emit 051h
-        _emit 056h
-        _emit 051h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 089h
-        _emit 064h
-        _emit 024h
-        _emit 008h
-        _emit 08Bh
-        _emit 0CCh
-        _emit 068h
-        _emit 004h
-        _emit 086h
-        _emit 009h
-        _emit 001h
-        _emit 0E8h
-        _emit 08Bh
-        _emit 088h
-        _emit 070h
-        _emit 000h
-        _emit 08Bh
-        _emit 044h
-        _emit 024h
-        _emit 010h
-        _emit 050h
-        _emit 08Bh
-        _emit 0CEh
-        _emit 0E8h
-        _emit 071h
-        _emit 032h
-        _emit 0E8h
-        _emit 0FFh
-        _emit 08Bh
-        _emit 04Ch
-        _emit 024h
-        _emit 010h
-        _emit 032h
-        _emit 0C0h
-        _emit 088h
-        _emit 046h
-        _emit 027h
-        _emit 088h
-        _emit 046h
-        _emit 02Ch
-        _emit 0C6h
-        _emit 046h
-        _emit 026h
-        _emit 001h
-        _emit 066h
-        _emit 0C7h
-        _emit 046h
-        _emit 024h
-        _emit 0FFh
-        _emit 0FFh
-        _emit 0C7h
-        _emit 006h
-        _emit 020h
-        _emit 0ADh
-        _emit 009h
-        _emit 001h
-        _emit 089h
-        _emit 04Eh
-        _emit 028h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 059h
-        _emit 0C2h
-        _emit 008h
-        _emit 000h
-    }
+public:
+	enum AIIdleTargetingType
+	{
+		LOOK_FOR_TARGETS,
+		DO_NOT_LOOK_FOR_TARGETS
+	};
+
+	AIIdleState(StateMachine *machine, AIIdleTargetingType targetingType);
+
+private:
+	unsigned short m_initialSleepOffset;
+	bool m_shouldLookForTargets;
+	bool m_inited;
+	AIIdleTargetingType m_targetingType;
+	bool m_bfmeIdleState2C;
+};
+
+// ??0AIIdleState@@QAE@PAVStateMachine@@W4AIIdleTargetingType@0@@Z
+AIIdleState::AIIdleState(StateMachine *machine, AIIdleTargetingType targetingType) :
+	State(machine, "AIIdleState")
+{
+	m_inited = false;
+	m_bfmeIdleState2C = false;
+	m_shouldLookForTargets = true;
+	m_initialSleepOffset = 0xFFFF;
+	m_vftable = &g_AIIdleStateVTable;
+	m_targetingType = targetingType;
 }
