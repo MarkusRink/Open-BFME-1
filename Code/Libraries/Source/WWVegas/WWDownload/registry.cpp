e1a02d67dbb858ff68e310db91aef6f106b48cf6
Gabe Borges <gbborges@proton.me>
2026-09-08 01:18:44 -0400

    feat(wwdownload): convert setStringInRegistry
    The retail REG_NONE literal at 0x01088918, the RegCreateKeyExA and RegSetValueExA calls, and callers at 0x00884300 identify 0x00883E90 as the WWDownload setStringInRegistry overload.
    The clean STLport ABI implementation sits in Code/Libraries/Source/WWVegas/WWDownload/registry.cpp and matches all 284 retail bytes.

---
+++ Code/Libraries/Source/WWVegas/WWDownload/registry.cpp
@@ -0,0 +1,91 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /D_STLP_USE_STATIC_LIB
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+namespace _STL
+{
+
+template <class Character>
+class char_traits
+{
+};
+
+template <class Character>
+class allocator
+{
+};
+
+template <bool threads, int instance>
+class __node_alloc
+{
+public:
+	static void _M_deallocate(void *pointer, unsigned int bytes);
+};
+
+template <class Pointer, class Value, class Alloc>
+class _STLP_alloc_proxy : public Alloc
+{
+public:
+	Pointer _M_data;
+
+	void deallocate(Pointer pointer, unsigned int count)
+	{
+		if (pointer != 0)
+		{
+			if (count > 128)
+				::operator delete(pointer);
+			else
+				__node_alloc<false, 0>::_M_deallocate(pointer, count);
+		}
+	}
+};
+
+template <class Character, class Traits, class Alloc>
+class basic_string
+{
+public:
+	Character *_M_start;
+	Character *_M_finish;
+	_STLP_alloc_proxy<Character *, Character, Alloc> _M_end_of_storage;
+
+	const Character *c_str(void) const
+	{
+		return _M_start;
+	}
+
+	unsigned int length(void) const
+	{
+		return (unsigned int)(_M_finish - _M_start);
+	}
+
+	~basic_string(void)
+	{
+		_M_end_of_storage.deallocate(_M_start,
+			(unsigned int)(_M_end_of_storage._M_data - _M_start));
+	}
+};
+
+}
+
+typedef _STL::basic_string<char, _STL::char_traits<char>, _STL::allocator<char> > RegistryString;
+
+bool setStringInRegistry( HKEY root, RegistryString path, RegistryString key, RegistryString val )
+{
+	HKEY handle;
+	unsigned long type;
+	unsigned long returnValue;
+	int size;
+
+	if ((returnValue = RegCreateKeyEx( root, path.c_str(), 0, "REG_NONE",
+	                                  REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL,
+	                                  &handle, NULL )) == ERROR_SUCCESS)
+	{
+		type = REG_SZ;
+		size = val.length() + 1;
+		returnValue = RegSetValueEx( handle, key.c_str(), 0, type,
+		                             (unsigned char *)val.c_str(), size );
+		RegCloseKey( handle );
+	}
+
+	return (returnValue == ERROR_SUCCESS);
+}
