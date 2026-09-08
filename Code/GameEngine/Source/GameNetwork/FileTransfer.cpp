030c39d1aa7a19a52c10751fc3ba2ca55cb737bf
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 08:28:33 -0400

    Convert GetAssetUsageFromMap to C++

--- Code/GameEngine/Source/GameNetwork/FileTransfer.cpp
+++ Code/GameEngine/Source/GameNetwork/FileTransfer.cpp
@@ -14,7 +14,6 @@ extern "C" {
 	void *GetPreviewFromMap(void *out, void *path);
 	void *GetArtPreviewFromMap(void *out, void *path);
 	void *GetPicPreviewFromMap(void *out, void *path);
-	void *GetAssetUsageFromMap(void *out, void *path);
 	void *GetReadmeFromMap(void *out, void *path);
 	int doFileTransfer(void *filename, void *ls, int mask);
 	int DoAnyMapTransfers(void *game);
@@ -457,105 +456,6 @@ L03_66DA4B:
 // has the same function building the same string.
 // Builds "%s\\assetusage.txt" from the map path's directory. Retail's name: the ZH reference
 // has the same function building the same string.
-__declspec(naked) void *GetAssetUsageFromMap(void *out, void *path)
-{
-	__asm {
-		push 0FFFFFFFFh
-		push 1044BC1h
-		mov eax, dword ptr fs:[0h]
-		push eax
-		mov dword ptr fs:[0h], esp
-		sub esp, 10h
-		push esi
-		xor esi, esi
-		mov dword ptr [esp+0Ch], esi
-		push ecx
-		lea eax,  [esp+2Ch]
-		mov dword ptr [esp+0Ch], esp
-		mov ecx, esp
-		push eax
-		mov dword ptr [esp+24h], 1h
-		__emit 0E8h
-		__emit 0D8h
-		__emit 09Ch
-		__emit 021h
-		__emit 000h   // call 0x887B60
-		lea ecx,  [esp+0Ch]
-		push ecx
-		__emit 0E8h
-		__emit 08Dh
-		__emit 062h
-		__emit 099h
-		__emit 0FFh   // call 0x411F
-		add esp, 8h
-		mov dword ptr [esp+4h], esi
-		mov eax, dword ptr [esp+8h]
-		cmp eax, esi
-		mov byte ptr [esp+1Ch], 3h
-		je L00_66DEAB
-		add eax, 8h
-		jmp L01_66DEB0
-L00_66DEAB:
-		mov eax, 107388Bh
-L01_66DEB0:
-		push eax
-		push ecx
-		mov dword ptr [esp+18h], esp
-		mov ecx, esp
-		push 111A360h
-		__emit 0E8h
-		__emit 0FEh
-		__emit 0ACh
-		__emit 021h
-		__emit 000h   // call 0x888BC0
-		lea edx,  [esp+0Ch]
-		push edx
-		__emit 0E8h
-		__emit 024h
-		__emit 0B1h
-		__emit 021h
-		__emit 000h   // call 0x888FF0
-		mov esi, dword ptr [esp+30h]
-		add esp, 0Ch
-		lea eax,  [esp+4h]
-		push eax
-		mov ecx, esi
-		__emit 0E8h
-		__emit 081h
-		__emit 09Ch
-		__emit 021h
-		__emit 000h   // call 0x887B60
-		lea ecx,  [esp+4h]
-		mov dword ptr [esp+0Ch], 1h
-		mov byte ptr [esp+1Ch], 2h
-		__emit 0E8h
-		__emit 04Bh
-		__emit 09Ah
-		__emit 021h
-		__emit 000h   // call 0x887940
-		lea ecx,  [esp+8h]
-		mov byte ptr [esp+1Ch], 1h
-		__emit 0E8h
-		__emit 03Dh
-		__emit 09Ah
-		__emit 021h
-		__emit 000h   // call 0x887940
-		lea ecx,  [esp+28h]
-		mov byte ptr [esp+1Ch], 0h
-		__emit 0E8h
-		__emit 02Fh
-		__emit 09Ah
-		__emit 021h
-		__emit 000h   // call 0x887940
-		mov ecx, dword ptr [esp+14h]
-		mov eax, esi
-		mov dword ptr fs:[0h], ecx
-		pop esi
-		add esp, 1Ch
-		ret
-	}
-}
-
 // Builds "%s\\readme.txt" from the map path's directory. Retail's name: the ZH reference
 // has the same function building the same string.
 __declspec(naked) void *GetReadmeFromMap(void *out, void *path)
