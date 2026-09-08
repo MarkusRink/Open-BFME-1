d15af7215b9b78c2b0a906d2fe4230dbb925c78d
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 10:14:32 -0400

    Convert attack pursue state constructor to C++

--- Code/GameEngine/Source/GameLogic/AI/AIAttackPursueTargetStateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/AIAttackPursueTargetStateCtorThunk.cpp
@@ -1,105 +1,68 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHsc /Ireference/shims/sweep /Ireference/shims/campaignmanagerascii /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /ICode/Libraries/Source/WWVegas/WWLib
+
+#include "Common/AsciiString.h"
 
 class StateMachine;
 
-// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/AIStateMachine.h
-class AIAttackPursueTargetState
+// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/Common/StateMachine.h
+class State
 {
 public:
-    AIAttackPursueTargetState(StateMachine*, bool, bool, bool);
+	virtual ~State();
+
+protected:
+	unsigned char m_head[0x20];
 };
 
-__declspec(naked) AIAttackPursueTargetState::AIAttackPursueTargetState(StateMachine*, bool, bool, bool)
+// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/AIStateMachine.h
+class AIInternalMoveToState : public State
 {
-    __asm {
-        _emit 51h
-        _emit 56h
-        _emit 51h
-        _emit 8Bh
-        _emit 0F1h
-        _emit 89h
-        _emit 64h
-        _emit 24h
-        _emit 08h
-        _emit 8Bh
-        _emit 0CCh
-        _emit 68h
-        _emit 20h
-        _emit 0A6h
-        _emit 09h
-        _emit 01h
-        _emit 0E8h
-        _emit 0ABh
-        _emit 94h
-        _emit 70h
-        _emit 00h
-        _emit 8Bh
-        _emit 44h
-        _emit 24h
-        _emit 10h
-        _emit 50h
-        _emit 8Bh
-        _emit 0CEh
-        _emit 0E8h
-        _emit 61h
-        _emit 2Ah
-        _emit 0EBh
-        _emit 0FFh
-        _emit 8Ah
-        _emit 4Ch
-        _emit 24h
-        _emit 10h
-        _emit 8Ah
-        _emit 54h
-        _emit 24h
-        _emit 14h
-        _emit 33h
-        _emit 0C0h
-        _emit 0C7h
-        _emit 06h
-        _emit 0C8h
-        _emit 0A5h
-        _emit 09h
-        _emit 01h
-        _emit 89h
-        _emit 46h
-        _emit 50h
-        _emit 89h
-        _emit 46h
-        _emit 54h
-        _emit 89h
-        _emit 46h
-        _emit 58h
-        _emit 89h
-        _emit 46h
-        _emit 5Ch
-        _emit 88h
-        _emit 46h
-        _emit 62h
-        _emit 8Ah
-        _emit 44h
-        _emit 24h
-        _emit 18h
-        _emit 88h
-        _emit 46h
-        _emit 64h
-        _emit 88h
-        _emit 4Eh
-        _emit 60h
-        _emit 88h
-        _emit 56h
-        _emit 61h
-        _emit 0C6h
-        _emit 46h
-        _emit 63h
-        _emit 01h
-        _emit 8Bh
-        _emit 0C6h
-        _emit 5Eh
-        _emit 59h
-        _emit 0C2h
-        _emit 10h
-        _emit 00h
-    }
-}
+public:
+	AIInternalMoveToState(StateMachine *machine, AsciiString name);
 
+private:
+	unsigned char m_body[0x2C];
+};
+
+struct Coord3D
+{
+	Coord3D() : x(0.0f), y(0.0f), z(0.0f) {}
+
+	float x;
+	float y;
+	float z;
+};
+
+// upstream source and layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/AIStateMachine.h
+class AIAttackPursueTargetState : public AIInternalMoveToState
+{
+public:
+	AIAttackPursueTargetState(StateMachine *machine, bool follow, bool attackingObject, bool forceAttacking);
+	virtual bool isAttack() const;
+
+private:
+	Coord3D m_prevVictimPos;
+	unsigned int m_approachTimestamp;
+	bool m_follow;
+	bool m_isAttackingObject;
+	bool m_stopIfInRange;
+	bool m_isInitialApproach;
+	bool m_isForceAttacking;
+};
+
+// ??0AIAttackPursueTargetState@@QAE@PAVStateMachine@@_N11@Z
+AIAttackPursueTargetState::AIAttackPursueTargetState(
+	StateMachine *machine,
+	bool follow,
+	bool attackingObject,
+	bool forceAttacking) :
+	AIInternalMoveToState(machine, "AIAttackPursueTargetState"),
+	m_prevVictimPos(),
+	m_approachTimestamp(0),
+	m_follow(follow),
+	m_isAttackingObject(attackingObject),
+	m_stopIfInRange(false),
+	m_isInitialApproach(true),
+	m_isForceAttacking(forceAttacking)
+{
+}
