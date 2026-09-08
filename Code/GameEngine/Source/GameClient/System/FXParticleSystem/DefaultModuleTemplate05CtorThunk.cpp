6df957936d3f32a535a55ed93ea079e33eae7e3f
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 06:47:41 -0400

    Convert DefaultModuleTemplate draw constructor to C++

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/DefaultModuleTemplate05CtorThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/DefaultModuleTemplate05CtorThunk.cpp
@@ -1,126 +1,54 @@
 // cl: /DNDEBUG /MD /EHsc
 
+// Open-BFME5: DefaultModuleTemplate (N=6) default constructor.
+// The retail body constructs the CategoryModuleTemplate<6> base followed by
+// its DefaultDrawModuleInfo subobject.
+
 namespace FXParticleSystem
 {
-template <int N>
-class DefaultModuleTemplate
+
+class ModuleTemplate
 {
 public:
-    DefaultModuleTemplate();
+	virtual ~ModuleTemplate();
 };
 
-template <int N>
-__declspec(naked) DefaultModuleTemplate<N>::DefaultModuleTemplate()
+template <int Category>
+class CategoryModuleInfo
 {
-    __asm {
-        _emit 06Ah
-        _emit 0FFh
-        _emit 068h
-        _emit 038h
-        _emit 0ADh
-        _emit 003h
-        _emit 001h
-        _emit 064h
-        _emit 0A1h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 050h
-        _emit 064h
-        _emit 089h
-        _emit 025h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 051h
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 0C7h
-        _emit 046h
-        _emit 004h
-        _emit 07Ch
-        _emit 0F9h
-        _emit 010h
-        _emit 001h
-        _emit 057h
-        _emit 089h
-        _emit 074h
-        _emit 024h
-        _emit 008h
-        _emit 0C7h
-        _emit 006h
-        _emit 050h
-        _emit 0FCh
-        _emit 010h
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 004h
-        _emit 04Ch
-        _emit 0FCh
-        _emit 010h
-        _emit 001h
-        _emit 08Dh
-        _emit 07Eh
-        _emit 008h
-        _emit 08Bh
-        _emit 0CFh
-        _emit 0C7h
-        _emit 044h
-        _emit 024h
-        _emit 014h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 0E8h
-        _emit 0ACh
-        _emit 07Fh
-        _emit 0A6h
-        _emit 0FFh
-        _emit 08Bh
-        _emit 04Ch
-        _emit 024h
-        _emit 00Ch
-        _emit 0C7h
-        _emit 007h
-        _emit 04Ch
-        _emit 009h
-        _emit 011h
-        _emit 001h
-        _emit 0C7h
-        _emit 006h
-        _emit 038h
-        _emit 009h
-        _emit 011h
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 004h
-        _emit 034h
-        _emit 009h
-        _emit 011h
-        _emit 001h
-        _emit 05Fh
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 064h
-        _emit 089h
-        _emit 00Dh
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 083h
-        _emit 0C4h
-        _emit 010h
-        _emit 0C3h
-    }
+public:
+	virtual void unusedVirtual();
+};
+
+template <int Category>
+class CategoryModuleTemplateBase : public ModuleTemplate, public CategoryModuleInfo<Category>
+{
+};
+
+template <int Category>
+class CategoryModuleTemplate : public CategoryModuleTemplateBase<Category>
+{
+};
+
+class DefaultDrawModuleInfo
+{
+public:
+	DefaultDrawModuleInfo();
+	virtual ~DefaultDrawModuleInfo();
+};
+
+template <int Category>
+class DefaultModuleTemplate : public CategoryModuleTemplate<Category>, public DefaultDrawModuleInfo
+{
+public:
+	DefaultModuleTemplate();
+};
+
+template <int Category>
+DefaultModuleTemplate<Category>::DefaultModuleTemplate()
+{
 }
 
-template class DefaultModuleTemplate<6>;
+template DefaultModuleTemplate<6>::DefaultModuleTemplate();
+
 }
