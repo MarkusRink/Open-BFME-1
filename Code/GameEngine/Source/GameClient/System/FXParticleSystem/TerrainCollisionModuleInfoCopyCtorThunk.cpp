9ff4620af41f601ae7d619c53015ef9b7dfa1098
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 10:08:42 -0400

    Convert terrain collision info copy constructor

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/TerrainCollisionModuleInfoCopyCtorThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/TerrainCollisionModuleInfoCopyCtorThunk.cpp
@@ -1,131 +1,62 @@
 // cl: /DNDEBUG /MD /EHsc
 
-namespace FXParticleSystem
-{
-class TerrainCollisionModuleInfo
+// Open-BFME5: TerrainCollisionModuleInfo copy constructor.
+
+template <typename Character>
+class StringBase
 {
 public:
-    TerrainCollisionModuleInfo(const TerrainCollisionModuleInfo &);
+	StringBase(const StringBase &);
 };
 
-__declspec(naked) TerrainCollisionModuleInfo::TerrainCollisionModuleInfo(const TerrainCollisionModuleInfo &)
+class BFMERetailAsciiString : private StringBase<char>
 {
-    __asm {
-        _emit 06Ah
-        _emit 0FFh
-        _emit 068h
-        _emit 038h
-        _emit 0AFh
-        _emit 003h
-        _emit 001h
-        _emit 064h
-        _emit 0A1h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 050h
-        _emit 064h
-        _emit 089h
-        _emit 025h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 051h
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 057h
-        _emit 089h
-        _emit 074h
-        _emit 024h
-        _emit 008h
-        _emit 08Bh
-        _emit 07Ch
-        _emit 024h
-        _emit 01Ch
-        _emit 08Dh
-        _emit 047h
-        _emit 004h
-        _emit 050h
-        _emit 08Dh
-        _emit 04Eh
-        _emit 004h
-        _emit 0C7h
-        _emit 044h
-        _emit 024h
-        _emit 018h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 0C7h
-        _emit 006h
-        _emit 0F8h
-        _emit 012h
-        _emit 011h
-        _emit 001h
-        _emit 0E8h
-        _emit 074h
-        _emit 094h
-        _emit 02Ah
-        _emit 000h
-        _emit 08Dh
-        _emit 04Fh
-        _emit 008h
-        _emit 08Bh
-        _emit 001h
-        _emit 08Dh
-        _emit 056h
-        _emit 008h
-        _emit 089h
-        _emit 002h
-        _emit 08Bh
-        _emit 041h
-        _emit 004h
-        _emit 089h
-        _emit 042h
-        _emit 004h
-        _emit 08Bh
-        _emit 049h
-        _emit 008h
-        _emit 089h
-        _emit 04Ah
-        _emit 008h
-        _emit 08Ah
-        _emit 057h
-        _emit 014h
-        _emit 08Bh
-        _emit 04Ch
-        _emit 024h
-        _emit 00Ch
-        _emit 088h
-        _emit 056h
-        _emit 014h
-        _emit 08Bh
-        _emit 047h
-        _emit 018h
-        _emit 089h
-        _emit 046h
-        _emit 018h
-        _emit 05Fh
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 064h
-        _emit 089h
-        _emit 00Dh
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 083h
-        _emit 0C4h
-        _emit 010h
-        _emit 0C2h
-        _emit 004h
-        _emit 000h
-    }
+public:
+	BFMERetailAsciiString(const BFMERetailAsciiString &that)
+		: StringBase<char>(that)
+	{
+	}
+};
+
+namespace FXParticleSystem
+{
+
+class TerrainCollisionModuleInfoBase
+{
+public:
+	TerrainCollisionModuleInfoBase(const TerrainCollisionModuleInfoBase &) {}
+	virtual ~TerrainCollisionModuleInfoBase() {}
+};
+
+struct TerrainCollisionValueBlock
+{
+	unsigned int m_a;
+	unsigned int m_b;
+	unsigned int m_c;
+};
+
+class TerrainCollisionModuleInfo : public TerrainCollisionModuleInfoBase
+{
+public:
+	TerrainCollisionModuleInfo(const TerrainCollisionModuleInfo &);
+	virtual ~TerrainCollisionModuleInfo();
+
+private:
+	BFMERetailAsciiString m_eventName;
+	TerrainCollisionValueBlock m_eventTime;
+	bool m_flag;
+	unsigned int m_cached;
+};
+
+// ??0TerrainCollisionModuleInfo@FXParticleSystem@@QAE@ABV01@@Z
+TerrainCollisionModuleInfo::TerrainCollisionModuleInfo(
+	const TerrainCollisionModuleInfo &that)
+	: TerrainCollisionModuleInfoBase(that),
+	  m_eventName(that.m_eventName),
+	  m_eventTime(that.m_eventTime),
+	  m_flag(that.m_flag),
+	  m_cached(that.m_cached)
+{
 }
+
 }
