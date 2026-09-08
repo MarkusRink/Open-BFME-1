f991b5cad8e1c5829764beb18d09b4b1cc085180
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 05:15:29 -0400

    Convert life event INI writer to C++

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
@@ -131,14 +131,18 @@ public:
     float m_y;
 };
 
+class U1CountedHolder;
+
 
 extern const float BfmeZeroRange;
 
 void b_005f8b40();
 void b_005f8fc0();
+void b_005fcb60();
 void b_005ee1d0();
 void b_005cb9f0();
 void u1Do_005C7410(void *ini, void *instance, void *store, U1ByteFlagged *value);
+void u1Do_005C71F0(void *ini, void *instance, void *store, U1CountedHolder *value);
 void u1Do_005C9030(void *ini, void *instance, void *store, U1Pair *value);
 void u4Next005F5120(INI *ini, void *instance, void *store, const void *value);
 void u4Next005F8AE0(INI *ini, void *instance, void *store, const void *value);
@@ -11234,379 +11238,31 @@ void CylindricalEmissionVelocityModuleTemplate::writeINI(File &file, unsigned in
     reinterpret_cast<FinishWriteFunction>(b_005ee1d0)(&file, &flags);
 }
 // ?writeINI@LifeEventModuleTemplate@FXParticleSystem@@UBEXAAVFile@@I@Z
-__declspec(naked) void LifeEventModuleTemplate::writeINI(File &file, unsigned int flags) const
+void LifeEventModuleTemplate::writeINI(File &file, unsigned int flags) const
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0xa6
-        __emit 0xc9
-        __emit 0x03
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x81
-        __emit 0xec
-        __emit 0xdc
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x56
-        __emit 0x57
-        __emit 0x8b
-        __emit 0xbc
-        __emit 0x24
-        __emit 0xf4
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x84
-        __emit 0x24
-        __emit 0xf8
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x57
-        __emit 0x56
-        __emit 0xe8
-        __emit 0x04
-        __emit 0xab
-        __emit 0xa3
-        __emit 0xff
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x0c
-        __emit 0x6a
-        __emit 0x01
-        __emit 0x6a
-        __emit 0x10
-        __emit 0x8d
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x1c
-        __emit 0xe8
-        __emit 0x8a
-        __emit 0x15
-        __emit 0xa3
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x94
-        __emit 0x24
-        __emit 0xf8
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x14
-        __emit 0x51
-        __emit 0x68
-        __emit 0x40
-        __emit 0x44
-        __emit 0x11
-        __emit 0x01
-        __emit 0x52
-        __emit 0x8d
-        __emit 0x44
-        __emit 0x24
-        __emit 0x20
-        __emit 0x50
-        __emit 0xc7
-        __emit 0x84
-        __emit 0x24
-        __emit 0xfc
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x09
-        __emit 0x66
-        __emit 0xa3
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x94
-        __emit 0x24
-        __emit 0x08
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x10
-        __emit 0x51
-        __emit 0x68
-        __emit 0x34
-        __emit 0x44
-        __emit 0x11
-        __emit 0x01
-        __emit 0x52
-        __emit 0x8d
-        __emit 0x44
-        __emit 0x24
-        __emit 0x30
-        __emit 0x50
-        __emit 0xe8
-        __emit 0x5d
-        __emit 0x88
-        __emit 0xa3
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x94
-        __emit 0x24
-        __emit 0x18
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x08
-        __emit 0x51
-        __emit 0x68
-        __emit 0x24
-        __emit 0x44
-        __emit 0x11
-        __emit 0x01
-        __emit 0x52
-        __emit 0x8d
-        __emit 0x44
-        __emit 0x24
-        __emit 0x40
-        __emit 0x50
-        __emit 0xe8
-        __emit 0xfa
-        __emit 0x46
-        __emit 0xa0
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x8c
-        __emit 0x24
-        __emit 0x28
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc6
-        __emit 0x09
-        __emit 0x56
-        __emit 0x68
-        __emit 0x10
-        __emit 0x44
-        __emit 0x11
-        __emit 0x01
-        __emit 0x51
-        __emit 0x8d
-        __emit 0x54
-        __emit 0x24
-        __emit 0x50
-        __emit 0x52
-        __emit 0xe8
-        __emit 0xdf
-        __emit 0x46
-        __emit 0xa0
-        __emit 0xff
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x40
-        __emit 0x8d
-        __emit 0x44
-        __emit 0x24
-        __emit 0x08
-        __emit 0x50
-        __emit 0x8d
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x18
-        __emit 0xe8
-        __emit 0x49
-        __emit 0xfc
-        __emit 0xa1
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x08
-        __emit 0x8b
-        __emit 0x40
-        __emit 0x04
-        __emit 0x8b
-        __emit 0x17
-        __emit 0x2b
-        __emit 0xc1
-        __emit 0x50
-        __emit 0x51
-        __emit 0x8b
-        __emit 0xcf
-        __emit 0xc6
-        __emit 0x84
-        __emit 0x24
-        __emit 0xf4
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x01
-        __emit 0xff
-        __emit 0x52
-        __emit 0x10
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x2b
-        __emit 0xc1
-        __emit 0x85
-        __emit 0xc9
-        __emit 0xc6
-        __emit 0x84
-        __emit 0x24
-        __emit 0xec
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x74
-        __emit 0x1c
-        __emit 0x3d
-        __emit 0x80
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x76
-        __emit 0x0b
-        __emit 0x51
-        __emit 0xe8
-        __emit 0x98
-        __emit 0x50
-        __emit 0x28
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0xeb
-        __emit 0x0a
-        __emit 0x50
-        __emit 0x51
-        __emit 0xe8
-        __emit 0xcc
-        __emit 0x17
-        __emit 0x23
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x08
-        __emit 0x8d
-        __emit 0x8c
-        __emit 0x24
-        __emit 0xf8
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x57
-        __emit 0xe8
-        __emit 0x57
-        __emit 0x93
-        __emit 0xa4
-        __emit 0xff
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x08
-        __emit 0x8d
-        __emit 0x8c
-        __emit 0x24
-        __emit 0x84
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc7
-        __emit 0x84
-        __emit 0x24
-        __emit 0xec
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xe8
-        __emit 0x2b
-        __emit 0xca
-        __emit 0xa2
-        __emit 0xff
-        __emit 0x8d
-        __emit 0x8c
-        __emit 0x24
-        __emit 0x84
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc7
-        __emit 0x84
-        __emit 0x24
-        __emit 0x84
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc4
-        __emit 0xeb
-        __emit 0x12
-        __emit 0x01
-        __emit 0xe8
-        __emit 0xca
-        __emit 0x28
-        __emit 0x24
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x8c
-        __emit 0x24
-        __emit 0xe4
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x81
-        __emit 0xc4
-        __emit 0xe8
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc2
-        __emit 0x08
-        __emit 0x00
-    }
-}
+    typedef _STL::basic_string<char, _STL::char_traits<char>, _STL::allocator<char> > StreamText;
+    typedef BfmeNarrowStreamText StreamText;
+    typedef void (__cdecl *BaseWriteFunction)(const void *self, File *file, unsigned int *flags);
+    typedef void (__cdecl *FinishWriteFunction)(File *file, unsigned int *flags);
 
+    reinterpret_cast<BaseWriteFunction>(b_005fcb60)(this, &file, &flags);
+
+    _STL::basic_ofstream<char, _STL::char_traits<char> > stream(0x10);
+    u1Do_005C9030((void *)&stream, (void *)flags, (void *)0x01114440,
+        (U1Pair *)((unsigned char *)this + 0x14));
+    u1Do_005C71F0((void *)&stream, (void *)flags, (void *)0x01114434,
+        (U1CountedHolder *)((unsigned char *)this + 0x10));
+    u1Do_005C7410((void *)&stream, (void *)flags, (void *)0x01114424,
+        (U1ByteFlagged *)((unsigned char *)this + 0x08));
+    u1Do_005C7410((void *)&stream, (void *)flags, (void *)0x01114410,
+        (U1ByteFlagged *)((unsigned char *)this + 0x09));
+
+    {
+        writeStreamText(file,
+            ((Rva005CB9F0StreamText *)((unsigned char *)&stream))->getText());
+    }
+    reinterpret_cast<FinishWriteFunction>(b_005ee1d0)(&file, &flags);
+}
 // ?writeINI@LightningDrawModuleTemplate@FXParticleSystem@@UBEXAAVFile@@I@Z
 __declspec(naked) void LightningDrawModuleTemplate::writeINI(File &file, unsigned int flags) const
 {
