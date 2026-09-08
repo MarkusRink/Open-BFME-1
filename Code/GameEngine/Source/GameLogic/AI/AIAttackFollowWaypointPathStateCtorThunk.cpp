7ef59b9b1066c97cfb4c02d5b5b339c5d1518533
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 10:39:05 -0400

    Convert attack waypoint state constructor to C++

--- Code/GameEngine/Source/GameLogic/AI/AIAttackFollowWaypointPathStateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/AIAttackFollowWaypointPathStateCtorThunk.cpp
@@ -1,219 +1,139 @@
 // cl: /DNDEBUG /MD /EHsc
-// readable body of ??0AIAttackFollowWaypointPathState@@QAE@PAVStateMachine@@_N@Z: Code/GameEngine/Source/GameLogic/AI/AIStates.cpp
+// Identity evidence: AIStateMachine's constructor calls this 201-byte body;
+// its two retail literals are "AIFollowWaypointPathState" and
+// "AIAttackFollowMachine", matching the upstream constructor exactly.
 
-class StateMachine;
+#include "../../../../Libraries/Source/WWVegas/WWLib/string_base.h"
 
-// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/AIStateMachine.h
-class AIAttackFollowWaypointPathState
+// One byte, like EA's Bool field.  Its inline construction retains the source
+// initializer boundary that VC6 used before setting up the following new-expression.
+class BfmeOrderedBool
 {
 public:
-    AIAttackFollowWaypointPathState(StateMachine*, bool);
+	__forceinline BfmeOrderedBool(bool value)
+	{
+		m_value = value;
+	}
+
+private:
+	volatile bool m_value;
 };
 
-__declspec(naked) AIAttackFollowWaypointPathState::AIAttackFollowWaypointPathState(StateMachine*, bool)
+class AsciiString
 {
-    __asm {
-        _emit 6Ah
-        _emit 0FFh
-        _emit 68h
-        _emit 0F3h
-        _emit 62h
-        _emit 00h
-        _emit 01h
-        _emit 64h
-        _emit 0A1h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 50h
-        _emit 64h
-        _emit 89h
-        _emit 25h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 83h
-        _emit 0ECh
-        _emit 08h
-        _emit 53h
-        _emit 56h
-        _emit 57h
-        _emit 51h
-        _emit 8Bh
-        _emit 0F1h
-        _emit 89h
-        _emit 64h
-        _emit 24h
-        _emit 14h
-        _emit 8Bh
-        _emit 0CCh
-        _emit 68h
-        _emit 08h
-        _emit 0A8h
-        _emit 09h
-        _emit 01h
-        _emit 89h
-        _emit 74h
-        _emit 24h
-        _emit 14h
-        _emit 0E8h
-        _emit 0FEh
-        _emit 4Eh
-        _emit 70h
-        _emit 00h
-        _emit 8Bh
-        _emit 44h
-        _emit 24h
-        _emit 28h
-        _emit 50h
-        _emit 8Bh
-        _emit 0CEh
-        _emit 0E8h
-        _emit 0B4h
-        _emit 0E4h
-        _emit 0EAh
-        _emit 0FFh
-        _emit 8Ah
-        _emit 4Ch
-        _emit 24h
-        _emit 28h
-        _emit 33h
-        _emit 0DBh
-        _emit 89h
-        _emit 5Eh
-        _emit 50h
-        _emit 89h
-        _emit 5Eh
-        _emit 54h
-        _emit 89h
-        _emit 5Eh
-        _emit 58h
-        _emit 89h
-        _emit 5Eh
-        _emit 5Ch
-        _emit 89h
-        _emit 5Eh
-        _emit 60h
-        _emit 89h
-        _emit 5Eh
-        _emit 64h
-        _emit 88h
-        _emit 5Eh
-        _emit 68h
-        _emit 88h
-        _emit 4Eh
-        _emit 69h
-        _emit 88h
-        _emit 5Eh
-        _emit 6Ah
-        _emit 6Ah
-        _emit 44h
-        _emit 89h
-        _emit 5Ch
-        _emit 24h
-        _emit 20h
-        _emit 0C7h
-        _emit 06h
-        _emit 20h
-        _emit 0B2h
-        _emit 09h
-        _emit 01h
-        _emit 0E8h
-        _emit 30h
-        _emit 0E2h
-        _emit 6Fh
-        _emit 00h
-        _emit 8Bh
-        _emit 0F8h
-        _emit 83h
-        _emit 0C4h
-        _emit 04h
-        _emit 89h
-        _emit 7Ch
-        _emit 24h
-        _emit 28h
-        _emit 3Bh
-        _emit 0FBh
-        _emit 0C6h
-        _emit 44h
-        _emit 24h
-        _emit 1Ch
-        _emit 01h
-        _emit 74h
-        _emit 21h
-        _emit 51h
-        _emit 89h
-        _emit 64h
-        _emit 24h
-        _emit 28h
-        _emit 8Bh
-        _emit 0CCh
-        _emit 68h
-        _emit 00h
-        _emit 0B2h
-        _emit 09h
-        _emit 01h
-        _emit 0E8h
-        _emit 9Dh
-        _emit 4Eh
-        _emit 70h
-        _emit 00h
-        _emit 8Bh
-        _emit 56h
-        _emit 1Ch
-        _emit 8Bh
-        _emit 42h
-        _emit 10h
-        _emit 50h
-        _emit 8Bh
-        _emit 0CFh
-        _emit 0E8h
-        _emit 77h
-        _emit 0A2h
-        _emit 0E8h
-        _emit 0FFh
-        _emit 0EBh
-        _emit 02h
-        _emit 33h
-        _emit 0C0h
-        _emit 89h
-        _emit 46h
-        _emit 6Ch
-        _emit 8Bh
-        _emit 10h
-        _emit 8Bh
-        _emit 0C8h
-        _emit 88h
-        _emit 5Ch
-        _emit 24h
-        _emit 1Ch
-        _emit 0FFh
-        _emit 52h
-        _emit 1Ch
-        _emit 8Bh
-        _emit 4Ch
-        _emit 24h
-        _emit 14h
-        _emit 5Fh
-        _emit 8Bh
-        _emit 0C6h
-        _emit 5Eh
-        _emit 64h
-        _emit 89h
-        _emit 0Dh
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 5Bh
-        _emit 83h
-        _emit 0C4h
-        _emit 14h
-        _emit 0C2h
-        _emit 08h
-        _emit 00h
-    }
-}
+public:
+	AsciiString(const char *text) : m_string(text) {}
 
+	StringBase<char> m_string;
+};
+
+class StateMachine;
+class Object;
+
+// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/AIStateMachine.h
+class AIInternalMoveToState
+{
+public:
+	AIInternalMoveToState(void *machine, AsciiString name);
+
+	virtual ~AIInternalMoveToState();
+	virtual void slot01();
+	virtual void slot02();
+	virtual void slot03();
+	virtual void slot04();
+	virtual void slot05();
+	virtual void slot06();
+	virtual void slot07();
+	virtual void slot08();
+	virtual void slot09();
+	virtual void slot10();
+	virtual void slot11();
+	virtual void slot12();
+	virtual void slot13();
+	virtual void slot14();
+	virtual void slot15();
+	virtual void slot16();
+	virtual void slot17();
+
+protected:
+	char m_baseGap04[0x18];
+	void *m_machine;
+	char m_baseTail20[0x50 - 0x20];
+};
+
+// Seven slots after the destructor place initDefaultState at retail slot +0x1c.
+class Rva00581F60SubMachine
+{
+public:
+	Rva00581F60SubMachine(Object *owner, AsciiString name);
+	virtual ~Rva00581F60SubMachine();
+	virtual void unused04();
+	virtual void unused08();
+	virtual void unused0C();
+	virtual void unused10();
+	virtual void unused14();
+	virtual void unused18();
+	virtual void initDefaultState();
+
+private:
+	char m_unreconstructed04[0x40];
+};
+
+// The existing neutral pin reaches the 0x00181F60 body.  The enclosing
+// constructor and its "AIAttackFollowMachine" literal prove its role here.
+typedef Rva00581F60SubMachine AIAttackMoveStateMachine;
+
+class BfmeStateMachineLayout
+{
+private:
+	char m_gap00[0x10];
+
+public:
+	Object *m_owner;
+
+	Object *getOwner() { return m_owner; }
+};
+
+extern int g_AIAttackFollowWaypointPathStateVTable;
+
+// upstream source: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source/GameLogic/AI/AIStates.cpp
+class AIAttackFollowWaypointPathState : public AIInternalMoveToState
+{
+public:
+	AIAttackFollowWaypointPathState(StateMachine *machine, bool asGroup);
+
+private:
+	volatile int m_groupOffsetX;
+	volatile int m_groupOffsetY;
+	volatile float m_angle;
+	volatile int m_framesSleeping;
+	void *volatile m_currentWaypoint;
+	void *volatile m_priorWaypoint;
+	volatile bool m_appendGoalPosition;
+	const volatile bool m_moveAsGroup;
+	BfmeOrderedBool m_isFollowWaypointPathState;
+	char m_pad6B;
+	AIAttackMoveStateMachine *m_attackFollowMachine;
+};
+
+// ??0AIAttackFollowWaypointPathState@@QAE@PAVStateMachine@@_N@Z
+AIAttackFollowWaypointPathState::AIAttackFollowWaypointPathState(
+	StateMachine *machine,
+	bool asGroup) :
+	AIInternalMoveToState(machine, AsciiString("AIFollowWaypointPathState")),
+	m_groupOffsetX(0),
+	m_groupOffsetY(0),
+	m_angle(0.0f),
+	m_framesSleeping(0),
+	m_currentWaypoint(0),
+	m_priorWaypoint(0),
+	m_appendGoalPosition(false),
+	m_moveAsGroup(asGroup),
+	m_isFollowWaypointPathState(false)
+{
+	*(int **)this = &g_AIAttackFollowWaypointPathStateVTable;
+	m_attackFollowMachine = new AIAttackMoveStateMachine(
+		((BfmeStateMachineLayout *)m_machine)->getOwner(),
+		AsciiString("AIAttackFollowMachine"));
+	m_attackFollowMachine->initDefaultState();
+}
