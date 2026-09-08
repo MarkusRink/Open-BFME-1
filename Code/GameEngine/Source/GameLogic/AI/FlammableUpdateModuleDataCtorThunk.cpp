004496f7c9ee94fb169d5b8067773c386c110a36
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 12:58:41 -0400

    Convert FlammableUpdate module data constructor

--- Code/GameEngine/Source/GameLogic/AI/FlammableUpdateModuleDataCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/FlammableUpdateModuleDataCtorThunk.cpp
@@ -1,195 +1,106 @@
 // cl: /DNDEBUG /MD /EHsc
-// readable body of ??0FlammableUpdateModuleData@@QAE@XZ: Code/GameEngine/Source/GameLogic/Object/Update/FlammableUpdate.cpp
+// Open-BFME5: FlammableUpdate module-data constructor.
+//
+// The named friend_newModuleData factory at retail 0x00118460 allocates 0x54
+// bytes and calls this constructor. Its matched destructor at 0x00294C40
+// independently fixes the string at +0x18 and FireFXList vector at +0x24.
 
-// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/Module/FlammableUpdate.h
-class FlammableUpdateModuleData
+class RetailLayoutString
 {
 public:
-    FlammableUpdateModuleData();
+	RetailLayoutString() : m_data( 0 ) {}
+	~RetailLayoutString() { releaseBuffer(); }
+	void clear() { releaseBuffer(); }
+
+private:
+	void releaseBuffer();
+	char *m_data;
 };
 
-__declspec(naked) FlammableUpdateModuleData::FlammableUpdateModuleData()
+class FlammableUpdateModuleDataMemberB
 {
-    __asm {
-        _emit 06Ah
-        _emit 0FFh
-        _emit 068h
-        _emit 07Eh
-        _emit 01Ch
-        _emit 001h
-        _emit 001h
-        _emit 064h
-        _emit 0A1h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 050h
-        _emit 064h
-        _emit 089h
-        _emit 025h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 051h
-        _emit 053h
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 057h
-        _emit 089h
-        _emit 074h
-        _emit 024h
-        _emit 00Ch
-        _emit 033h
-        _emit 0DBh
-        _emit 08Dh
-        _emit 04Eh
-        _emit 018h
-        _emit 0C7h
-        _emit 006h
-        _emit 070h
-        _emit 0EFh
-        _emit 00Bh
-        _emit 001h
-        _emit 089h
-        _emit 05Ch
-        _emit 024h
-        _emit 018h
-        _emit 089h
-        _emit 019h
-        _emit 08Dh
-        _emit 07Eh
-        _emit 024h
-        _emit 089h
-        _emit 01Fh
-        _emit 089h
-        _emit 05Fh
-        _emit 004h
-        _emit 089h
-        _emit 05Fh
-        _emit 008h
-        _emit 0C6h
-        _emit 044h
-        _emit 024h
-        _emit 018h
-        _emit 002h
-        _emit 089h
-        _emit 05Eh
-        _emit 008h
-        _emit 089h
-        _emit 05Eh
-        _emit 00Ch
-        _emit 089h
-        _emit 05Eh
-        _emit 010h
-        _emit 089h
-        _emit 05Eh
-        _emit 014h
-        _emit 0E8h
-        _emit 0CFh
-        _emit 02Dh
-        _emit 05Fh
-        _emit 000h
-        _emit 0C7h
-        _emit 046h
-        _emit 01Ch
-        _emit 000h
-        _emit 000h
-        _emit 0A0h
-        _emit 041h
-        _emit 0C7h
-        _emit 046h
-        _emit 020h
-        _emit 019h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 08Bh
-        _emit 047h
-        _emit 004h
-        _emit 08Bh
-        _emit 00Fh
-        _emit 050h
-        _emit 051h
-        _emit 08Bh
-        _emit 0CFh
-        _emit 0E8h
-        _emit 0F0h
-        _emit 017h
-        _emit 0D9h
-        _emit 0FFh
-        _emit 08Bh
-        _emit 04Ch
-        _emit 024h
-        _emit 010h
-        _emit 088h
-        _emit 05Eh
-        _emit 030h
-        _emit 088h
-        _emit 05Eh
-        _emit 031h
-        _emit 088h
-        _emit 05Eh
-        _emit 032h
-        _emit 088h
-        _emit 05Eh
-        _emit 033h
-        _emit 088h
-        _emit 05Eh
-        _emit 034h
-        _emit 088h
-        _emit 05Eh
-        _emit 035h
-        _emit 089h
-        _emit 05Eh
-        _emit 038h
-        _emit 088h
-        _emit 05Eh
-        _emit 044h
-        _emit 089h
-        _emit 05Eh
-        _emit 04Ch
-        _emit 089h
-        _emit 05Eh
-        _emit 050h
-        _emit 05Fh
-        _emit 0C7h
-        _emit 046h
-        _emit 03Ch
-        _emit 000h
-        _emit 000h
-        _emit 048h
-        _emit 043h
-        _emit 0C7h
-        _emit 046h
-        _emit 040h
-        _emit 000h
-        _emit 000h
-        _emit 070h
-        _emit 042h
-        _emit 0C7h
-        _emit 046h
-        _emit 048h
-        _emit 0FFh
-        _emit 0FFh
-        _emit 0FFh
-        _emit 0FFh
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 05Bh
-        _emit 064h
-        _emit 089h
-        _emit 00Dh
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 083h
-        _emit 0C4h
-        _emit 010h
-        _emit 0C3h
-    }
+public:
+	FlammableUpdateModuleDataMemberB()
+		: m_begin( 0 ), m_end( 0 ), m_capacity( 0 )
+	{
+	}
+	~FlammableUpdateModuleDataMemberB();
+
+	void clear()
+	{
+		erase( m_begin, m_end );
+	}
+
+private:
+	void erase( void *first, void *last );
+	void *m_begin;
+	void *m_end;
+	void *m_capacity;
+};
+
+class __declspec(novtable) FlammableUpdateModuleDataBase
+{
+public:
+	virtual ~FlammableUpdateModuleDataBase();
+
+protected:
+	unsigned int m_moduleData;
+	unsigned int m_burnedDelay;
+	unsigned int m_aflameDuration;
+	unsigned int m_aflameDamageDelay;
+	int m_aflameDamageAmount;
+};
+
+class FlammableUpdateModuleData : public FlammableUpdateModuleDataBase
+{
+public:
+	FlammableUpdateModuleData();
+	virtual ~FlammableUpdateModuleData();
+
+private:
+	RetailLayoutString m_burningSoundName;
+	float m_flameDamageLimitData;
+	unsigned int m_flameDamageExpirationDelay;
+	FlammableUpdateModuleDataMemberB m_fireFXList;
+	unsigned char m_enabled;
+	unsigned char m_affectSelf;
+	unsigned char m_affectAllies;
+	unsigned char m_affectEnemies;
+	unsigned char m_affectNeutral;
+	unsigned char m_affectDead;
+	unsigned char m_pad36[ 2 ];
+	unsigned int m_fxDelay;
+	float m_range;
+	float m_height;
+	unsigned char m_looping;
+	unsigned char m_pad45[ 3 ];
+	int m_damageType;
+	unsigned int m_extra0;
+	unsigned int m_extra1;
+};
+
+// ??0FlammableUpdateModuleData@@QAE@XZ
+FlammableUpdateModuleData::FlammableUpdateModuleData()
+{
+	m_burnedDelay = 0;
+	m_aflameDuration = 0;
+	m_aflameDamageDelay = 0;
+	m_aflameDamageAmount = 0;
+	m_burningSoundName.clear();
+	m_flameDamageLimitData = 20.0f;
+	m_flameDamageExpirationDelay = 25;
+	m_fireFXList.clear();
+	m_enabled = 0;
+	m_affectSelf = 0;
+	m_affectAllies = 0;
+	m_affectEnemies = 0;
+	m_affectNeutral = 0;
+	m_affectDead = 0;
+	m_fxDelay = 0;
+	m_looping = 0;
+	m_extra0 = 0;
+	m_extra1 = 0;
+	m_range = 200.0f;
+	m_height = 60.0f;
+	m_damageType = -1;
 }
