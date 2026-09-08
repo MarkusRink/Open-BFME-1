aa59a370da7c9bd11f73d82da49485888c582342
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 07:35:44 -0400

    Convert default physics INI factory to C++

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/ConcreteModuleClassDefaultModuleTag2CreateTemplateFromINIThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/ConcreteModuleClassDefaultModuleTag2CreateTemplateFromINIThunk.cpp
@@ -1,158 +1,68 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHsc /O2 /Ob2
 
 class INI;
 
 namespace FXParticleSystem
 {
-template<int Category>
-class DefaultModuleTag
-{
-};
+
+void __cdecl operator delete(void *);
+template<int Category> class DefaultModuleTag {};
 
 template<int Category>
 class DefaultModuleTemplate
 {
+public:
+	void parse(INI *ini);
+};
+
+class DefaultModuleTemplate3CtorShim
+{
+public:
+	void construct();
+};
+
+extern "C" char DefaultModuleTemplate3Concrete_vtbl0;
+extern "C" char DefaultModuleTemplate3Concrete_vtbl4;
+extern "C" char DefaultModuleTemplate3Concrete_vtbl8;
+
+class DefaultModuleTemplate3Allocation
+{
+public:
+	DefaultModuleTemplate3Allocation()
+	{
+		((DefaultModuleTemplate3CtorShim *)this)->construct();
+		*(void **)((char *)this + 0) = &DefaultModuleTemplate3Concrete_vtbl0;
+		*(void **)((char *)this + 4) = &DefaultModuleTemplate3Concrete_vtbl4;
+		*(void **)((char *)this + 8) = &DefaultModuleTemplate3Concrete_vtbl8;
+	}
+
+private:
+	unsigned char bytes_[0x28];
 };
 
 template<class Tag>
 class ConcreteModuleClass
 {
 public:
-    virtual DefaultModuleTemplate<3> *createTemplate(INI *) const;
+	virtual DefaultModuleTemplate<3> *createTemplate(INI *) const;
 };
 
 template<class Tag>
-__declspec(naked) DefaultModuleTemplate<3> *ConcreteModuleClass<Tag>::createTemplate(INI *) const
+DefaultModuleTemplate<3> *ConcreteModuleClass<Tag>::createTemplate(INI *ini) const
 {
-    __asm {
-        _emit 06Ah
-        _emit 0FFh
-        _emit 068h
-        _emit 053h
-        _emit 0B7h
-        _emit 003h
-        _emit 001h
-        _emit 064h
-        _emit 0A1h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 050h
-        _emit 064h
-        _emit 089h
-        _emit 025h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 051h
-        _emit 056h
-        _emit 06Ah
-        _emit 028h
-        _emit 0E8h
-        _emit 0B2h
-        _emit 0DBh
-        _emit 029h
-        _emit 000h
-        _emit 08Bh
-        _emit 0F0h
-        _emit 083h
-        _emit 0C4h
-        _emit 004h
-        _emit 089h
-        _emit 074h
-        _emit 024h
-        _emit 004h
-        _emit 085h
-        _emit 0F6h
-        _emit 0C7h
-        _emit 044h
-        _emit 024h
-        _emit 010h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 074h
-        _emit 01Dh
-        _emit 08Bh
-        _emit 0CEh
-        _emit 0E8h
-        _emit 0E0h
-        _emit 070h
-        _emit 0A6h
-        _emit 0FFh
-        _emit 0C7h
-        _emit 006h
-        _emit 0C4h
-        _emit 00Fh
-        _emit 011h
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 004h
-        _emit 0C0h
-        _emit 00Fh
-        _emit 011h
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 008h
-        _emit 0ACh
-        _emit 00Fh
-        _emit 011h
-        _emit 001h
-        _emit 0EBh
-        _emit 002h
-        _emit 033h
-        _emit 0F6h
-        _emit 089h
-        _emit 074h
-        _emit 024h
-        _emit 004h
-        _emit 08Bh
-        _emit 044h
-        _emit 024h
-        _emit 018h
-        _emit 050h
-        _emit 08Bh
-        _emit 0CEh
-        _emit 0C7h
-        _emit 044h
-        _emit 024h
-        _emit 014h
-        _emit 001h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 0E8h
-        _emit 05Dh
-        _emit 061h
-        _emit 0A4h
-        _emit 0FFh
-        _emit 08Bh
-        _emit 04Ch
-        _emit 024h
-        _emit 008h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 064h
-        _emit 089h
-        _emit 00Dh
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 083h
-        _emit 0C4h
-        _emit 010h
-        _emit 0C2h
-        _emit 004h
-        _emit 000h
-    }
+	DefaultModuleTemplate<3> *result = (DefaultModuleTemplate<3> *)
+		new DefaultModuleTemplate3Allocation;
+	struct ParseScope
+	{
+		ParseScope(DefaultModuleTemplate<3> *value) : value_(value) {}
+		~ParseScope() { if (value_) operator delete(value_); }
+		DefaultModuleTemplate<3> *value_;
+	} scope(result);
+	result->parse(ini);
+	scope.value_ = 0;
+	return result;
 }
 
 template class ConcreteModuleClass<DefaultModuleTag<3> >;
+
 }
