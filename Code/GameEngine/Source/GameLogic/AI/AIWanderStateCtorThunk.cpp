aa76d6676ebdd2710256e20f38c169644c22eb05
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 10:04:10 -0400

    Convert AIWanderState constructor to C++

--- Code/GameEngine/Source/GameLogic/AI/AIWanderStateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/AIWanderStateCtorThunk.cpp
@@ -1,98 +1,56 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHsc /Ireference/shims/sweep /Ireference/shims/campaignmanagerascii /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /ICode/Libraries/Source/WWVegas/WWLib
+
+#include "Common/AsciiString.h"
 
 class StateMachine;
 
 // upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/AIStateMachine.h
-class AIWanderState
+class AIInternalMoveToState
 {
 public:
-    AIWanderState(StateMachine *);
+	AIInternalMoveToState(StateMachine *machine, AsciiString name);
 };
 
-__declspec(naked) AIWanderState::AIWanderState(StateMachine *)
+extern int g_AIWanderStateVTable;
+
+// upstream source: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include/GameLogic/AIStateMachine.h
+// The declared AIFollowWaypointPathState constructor is inline in the retail header;
+// these fields model its portion of the derived object after inlining.
+class AIWanderState : public AIInternalMoveToState
 {
-    __asm {
-        _emit 051h
-        _emit 056h
-        _emit 051h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 089h
-        _emit 064h
-        _emit 024h
-        _emit 008h
-        _emit 08Bh
-        _emit 0CCh
-        _emit 068h
-        _emit 008h
-        _emit 0A8h
-        _emit 009h
-        _emit 001h
-        _emit 0E8h
-        _emit 0ABh
-        _emit 090h
-        _emit 070h
-        _emit 000h
-        _emit 08Bh
-        _emit 044h
-        _emit 024h
-        _emit 010h
-        _emit 050h
-        _emit 08Bh
-        _emit 0CEh
-        _emit 0E8h
-        _emit 061h
-        _emit 026h
-        _emit 0EBh
-        _emit 0FFh
-        _emit 033h
-        _emit 0C0h
-        _emit 089h
-        _emit 046h
-        _emit 050h
-        _emit 089h
-        _emit 046h
-        _emit 054h
-        _emit 089h
-        _emit 046h
-        _emit 058h
-        _emit 089h
-        _emit 046h
-        _emit 05Ch
-        _emit 089h
-        _emit 046h
-        _emit 060h
-        _emit 089h
-        _emit 046h
-        _emit 064h
-        _emit 088h
-        _emit 046h
-        _emit 068h
-        _emit 088h
-        _emit 046h
-        _emit 069h
-        _emit 089h
-        _emit 046h
-        _emit 06Ch
-        _emit 089h
-        _emit 046h
-        _emit 070h
-        _emit 0C6h
-        _emit 046h
-        _emit 06Ah
-        _emit 001h
-        _emit 0C7h
-        _emit 006h
-        _emit 0A8h
-        _emit 0A8h
-        _emit 009h
-        _emit 001h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 059h
-        _emit 0C2h
-        _emit 004h
-        _emit 000h
-    }
+public:
+	AIWanderState(StateMachine *machine);
+
+private:
+	int *volatile m_vftable;
+	char m_baseFields[0x4C];
+	volatile int m_groupOffsetX;
+	volatile int m_groupOffsetY;
+	volatile int m_angle;
+	volatile int m_framesSleeping;
+	volatile int m_currentWaypoint;
+	volatile int m_priorWaypoint;
+	volatile bool m_appendGoalPosition;
+	volatile bool m_moveAsGroup;
+	volatile bool m_isFollowWaypointPathState;
+	volatile int m_waitFrames;
+	volatile int m_timer;
+};
+
+// ??0AIWanderState@@QAE@PAVStateMachine@@@Z
+AIWanderState::AIWanderState(StateMachine *machine) :
+	AIInternalMoveToState(machine, "AIFollowWaypointPathState")
+{
+	m_groupOffsetX = 0;
+	m_groupOffsetY = 0;
+	m_angle = 0;
+	m_framesSleeping = 0;
+	m_currentWaypoint = 0;
+	m_priorWaypoint = 0;
+	m_appendGoalPosition = false;
+	m_moveAsGroup = false;
+	m_waitFrames = 0;
+	m_timer = 0;
+	m_isFollowWaypointPathState = true;
+	m_vftable = &g_AIWanderStateVTable;
 }
