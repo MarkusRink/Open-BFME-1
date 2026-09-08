c8bc932fe2139677313b6318f6019051608b6acc
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 02:41:27 -0400

    ww3d: convert segmented-line render helper

--- Code/Libraries/Source/WWVegas/WW3D2/SegmentedLineRenderSegLineThunk.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/SegmentedLineRenderSegLineThunk.cpp
@@ -1,95 +1,140 @@
-// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2/rinfo.h
+// cl: /DNDEBUG /MD /EHsc
+
 class RenderInfoClass
 {
 };
 
-// upstream layout: reference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2/segline.h
+class Matrix3D
+{
+    float Values[12];
+};
+
+class Vector3
+{
+};
+
+class Vector4
+{
+};
+
+class SphereClass
+{
+    float Values[4];
+};
+
+class SegLineRendererClass
+{
+public:
+    void Render(RenderInfoClass &, const Matrix3D &, unsigned int, Vector3 *,
+        const SphereClass &, Vector4 * = 0);
+};
+
+template <class Type>
+class SimpleDynVecClass
+{
+public:
+    virtual ~SimpleDynVecClass();
+
+    Type *Vector;
+    int VectorMax;
+    int ActiveCount;
+};
+
 class SegmentedLineClass
 {
+public:
+    virtual void Reserved_0();
+    virtual void Reserved_1();
+    virtual void Reserved_2();
+    virtual void Reserved_3();
+    virtual void Reserved_4();
+    virtual void Reserved_5();
+    virtual void Reserved_6();
+    virtual void Reserved_7();
+    virtual void Reserved_8();
+    virtual void Reserved_9();
+    virtual void Reserved_10();
+    virtual void Reserved_11();
+    virtual void Reserved_12();
+    virtual void Reserved_13();
+    virtual void Reserved_14();
+    virtual void Reserved_15();
+    virtual void Reserved_16();
+    virtual void Reserved_17();
+    virtual void Reserved_18();
+    virtual void Reserved_19();
+    virtual void Reserved_20();
+    virtual void Reserved_21();
+    virtual void Reserved_22();
+    virtual void Reserved_23();
+    virtual void Reserved_24();
+    virtual void Reserved_25();
+    virtual void Reserved_26();
+    virtual void Reserved_27();
+    virtual void Reserved_28();
+    virtual void Reserved_29();
+    virtual void Reserved_30();
+    virtual void Reserved_31();
+    virtual void Reserved_32();
+    virtual void Reserved_33();
+    virtual void Reserved_34();
+    virtual void Reserved_35();
+    virtual void Reserved_36();
+    virtual void Reserved_37();
+    virtual void Reserved_38();
+    virtual void Reserved_39();
+    virtual void Reserved_40();
+    virtual void Reserved_41();
+    virtual void Reserved_42();
+    virtual void Reserved_43();
+    virtual void Reserved_44();
+    virtual void Reserved_45();
+    virtual void Reserved_46();
+    virtual void Reserved_47();
+    virtual void Reserved_48();
+    virtual void Reserved_49();
+    virtual void Reserved_50();
+    virtual void Reserved_51();
+    virtual void Reserved_52();
+    virtual void Reserved_53();
+    virtual void Reserved_54();
+    virtual void Reserved_55();
+    virtual void Reserved_56();
+    virtual void Reserved_57();
+    virtual void Reserved_58();
+    virtual void Reserved_59();
+    virtual void Reserved_60();
+    virtual void Reserved_61();
+    virtual void Reserved_62();
+    virtual void Reserved_63();
+    virtual void Reserved_64();
+    virtual void Reserved_65();
+    virtual void Get_Obj_Space_Bounding_Sphere(SphereClass &);
+
+private:
+    unsigned char Prefix[0x14];
+    Matrix3D Transform;
+    unsigned char BeforePointLocations[0x88];
+    SimpleDynVecClass<Vector3> PointLocations;
+    SegLineRendererClass LineRenderer;
+
 protected:
     void Render_Seg_Line(RenderInfoClass &);
 };
 
-__declspec(naked) void SegmentedLineClass::Render_Seg_Line(RenderInfoClass &)
+void SegmentedLineClass::Render_Seg_Line(RenderInfoClass &rinfo)
 {
-    __asm {
-        _emit 083h
-        _emit 0ECh
-        _emit 010h
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 083h
-        _emit 0BEh
-        _emit 0DCh
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 002h
-        _emit 07Ch
-        _emit 038h
-        _emit 08Bh
-        _emit 006h
-        _emit 08Dh
-        _emit 04Ch
-        _emit 024h
-        _emit 004h
-        _emit 051h
-        _emit 08Bh
-        _emit 0CEh
-        _emit 0FFh
-        _emit 090h
-        _emit 008h
-        _emit 001h
-        _emit 000h
-        _emit 000h
-        _emit 08Bh
-        _emit 086h
-        _emit 0D4h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 08Bh
-        _emit 08Eh
-        _emit 0DCh
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 06Ah
-        _emit 000h
-        _emit 08Dh
-        _emit 054h
-        _emit 024h
-        _emit 008h
-        _emit 052h
-        _emit 050h
-        _emit 08Bh
-        _emit 044h
-        _emit 024h
-        _emit 024h
-        _emit 051h
-        _emit 08Dh
-        _emit 056h
-        _emit 018h
-        _emit 052h
-        _emit 050h
-        _emit 08Dh
-        _emit 08Eh
-        _emit 0E0h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 0E8h
-        _emit 089h
-        _emit 000h
-        _emit 001h
-        _emit 000h
-        _emit 05Eh
-        _emit 083h
-        _emit 0C4h
-        _emit 010h
-        _emit 0C2h
-        _emit 004h
-        _emit 000h
+    if (PointLocations.ActiveCount < 2) {
+        return;
     }
+
+    SphereClass boundingSphere;
+    Get_Obj_Space_Bounding_Sphere(boundingSphere);
+
+    LineRenderer.Render(
+        rinfo,
+        Transform,
+        PointLocations.ActiveCount,
+        PointLocations.Vector,
+        boundingSphere);
 }
