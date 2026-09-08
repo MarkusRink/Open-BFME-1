350213aadf4ae79a56afbaf8045e485c02ac175d
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 13:47:59 -0400

    Convert FadeAndDie ornament module data constructor

--- Code/GameEngine/Source/Common/RTS/FadeAndDieOrnamentUpdateModuleDataCtorThunk.cpp
+++ Code/GameEngine/Source/Common/RTS/FadeAndDieOrnamentUpdateModuleDataCtorThunk.cpp
@@ -1,141 +1,73 @@
 // cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: FadeAndDieOrnamentUpdate module-data constructor.
+//
+// The named friend_newModuleData factory at retail 0x00125BD0 allocates 0x38
+// bytes and calls this body. Its matched destructor at 0x00126E70 independently
+// fixes the retail AsciiString at +0x08.
 
-class FadeAndDieOrnamentUpdateModuleData
+class BFMERetailAsciiString
 {
 public:
-    FadeAndDieOrnamentUpdateModuleData();
+	BFMERetailAsciiString() : m_data( 0 ) {}
+	~BFMERetailAsciiString() { releaseBuffer(); }
+	void clear() { releaseBuffer(); }
+
+private:
+	void releaseBuffer();
+	char *m_data;
 };
 
-__declspec(naked) FadeAndDieOrnamentUpdateModuleData::FadeAndDieOrnamentUpdateModuleData()
+struct FadeAndDieSettings
 {
-    __asm {
-        _emit 6Ah
-        _emit 0FFh
-        _emit 68h
-        _emit 93h
-        _emit 17h
-        _emit 00h
-        _emit 01h
-        _emit 64h
-        _emit 0A1h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 50h
-        _emit 64h
-        _emit 89h
-        _emit 25h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 51h
-        _emit 53h
-        _emit 56h
-        _emit 8Bh
-        _emit 0F1h
-        _emit 89h
-        _emit 74h
-        _emit 24h
-        _emit 08h
-        _emit 33h
-        _emit 0C0h
-        _emit 8Dh
-        _emit 4Eh
-        _emit 08h
-        _emit 0C7h
-        _emit 06h
-        _emit 38h
-        _emit 0E8h
-        _emit 08h
-        _emit 01h
-        _emit 89h
-        _emit 44h
-        _emit 24h
-        _emit 14h
-        _emit 89h
-        _emit 01h
-        _emit 0BAh
-        _emit 00h
-        _emit 00h
-        _emit 80h
-        _emit 3Fh
-        _emit 89h
-        _emit 56h
-        _emit 0Ch
-        _emit 89h
-        _emit 56h
-        _emit 10h
-        _emit 89h
-        _emit 56h
-        _emit 14h
-        _emit 0BBh
-        _emit 01h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 83h
-        _emit 0CAh
-        _emit 0FFh
-        _emit 89h
-        _emit 46h
-        _emit 18h
-        _emit 89h
-        _emit 5Eh
-        _emit 1Ch
-        _emit 89h
-        _emit 5Eh
-        _emit 20h
-        _emit 89h
-        _emit 5Eh
-        _emit 24h
-        _emit 89h
-        _emit 56h
-        _emit 28h
-        _emit 89h
-        _emit 56h
-        _emit 2Ch
-        _emit 89h
-        _emit 46h
-        _emit 30h
-        _emit 88h
-        _emit 5Ch
-        _emit 24h
-        _emit 14h
-        _emit 89h
-        _emit 46h
-        _emit 18h
-        _emit 89h
-        _emit 46h
-        _emit 30h
-        _emit 0E8h
-        _emit 0A7h
-        _emit 1Dh
-        _emit 76h
-        _emit 00h
-        _emit 8Bh
-        _emit 4Ch
-        _emit 24h
-        _emit 0Ch
-        _emit 88h
-        _emit 5Eh
-        _emit 34h
-        _emit 8Bh
-        _emit 0C6h
-        _emit 5Eh
-        _emit 5Bh
-        _emit 64h
-        _emit 89h
-        _emit 0Dh
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 83h
-        _emit 0C4h
-        _emit 10h
-        _emit 0C3h
-    }
-}
+	FadeAndDieSettings()
+		: m_zero18( 0 ), m_one1C( 1 ), m_one20( 1 ), m_one24( 1 ),
+		  m_negative28( -1 ), m_negative2C( -1 ), m_zero30( 0 )
+	{
+	}
 
+	unsigned int m_zero18;
+	unsigned int m_one1C;
+	unsigned int m_one20;
+	unsigned int m_one24;
+	int m_negative28;
+	int m_negative2C;
+	unsigned int m_zero30;
+};
+
+class __declspec(novtable) FadeAndDieOrnamentUpdateModuleDataBase
+{
+public:
+	virtual ~FadeAndDieOrnamentUpdateModuleDataBase();
+
+private:
+	unsigned int m_moduleData;
+};
+
+class FadeAndDieOrnamentUpdateModuleData
+	: public FadeAndDieOrnamentUpdateModuleDataBase
+{
+public:
+	FadeAndDieOrnamentUpdateModuleData();
+	virtual ~FadeAndDieOrnamentUpdateModuleData();
+
+private:
+	BFMERetailAsciiString m_name;
+	float m_value0C;
+	float m_value10;
+	float m_value14;
+	FadeAndDieSettings m_settings;
+	unsigned char m_enabled;
+	unsigned char m_pad35[ 3 ];
+};
+
+// ??0FadeAndDieOrnamentUpdateModuleData@@QAE@XZ
+FadeAndDieOrnamentUpdateModuleData::FadeAndDieOrnamentUpdateModuleData()
+	: m_value0C( 1.0f ),
+	  m_value10( 1.0f ),
+	  m_value14( 1.0f )
+{
+	m_settings.m_zero18 = 0;
+	m_settings.m_zero30 = 0;
+	m_name.clear();
+	m_enabled = 1;
+}
