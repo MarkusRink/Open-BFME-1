f7f69f5c8ad6c3f2a810e2aaf3f645c39430625f
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 11:42:57 -0400

    Convert PlayerUpgrade module-data constructor to C++

--- Code/GameEngine/Source/GameLogic/AI/PlayerUpgradeSpecialPowerModuleDataCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/PlayerUpgradeSpecialPowerModuleDataCtorThunk.cpp
@@ -1,108 +1,59 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /DWIN32 /MD /EHsc /D_STLP_USE_STATIC_LIB
+// stlport
+// Open-BFME5: PlayerUpgradeSpecialPower module-data constructor.
+//
+// The named friend_newModuleData factory at retail 0x00121370 allocates 0x21C
+// bytes and calls this constructor.  The final twelve bytes are an STLport
+// vector<AsciiString>: its destructor at 0x000658A0 walks four-byte strings
+// from begin to end and frees the begin/capacity allocation afterward.
 
-class PlayerUpgradeSpecialPowerModuleData
+#include <vector>
+
+class AsciiString
 {
 public:
-    PlayerUpgradeSpecialPowerModuleData();
+	~AsciiString();
+
+private:
+	char *m_text;
 };
 
-__declspec(naked) PlayerUpgradeSpecialPowerModuleData::PlayerUpgradeSpecialPowerModuleData()
+class BfmeSpecialPowerModuleDataBase
 {
-    __asm {
-        _emit 6Ah
-        _emit 0FFh
-        _emit 68h
-        _emit 46h
-        _emit 0F9h
-        _emit 00h
-        _emit 01h
-        _emit 64h
-        _emit 0A1h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 50h
-        _emit 64h
-        _emit 89h
-        _emit 25h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 51h
-        _emit 56h
-        _emit 8Bh
-        _emit 0F1h
-        _emit 89h
-        _emit 74h
-        _emit 24h
-        _emit 04h
-        _emit 0E8h
-        _emit 12h
-        _emit 1Ah
-        _emit 0DBh
-        _emit 0FFh
-        _emit 33h
-        _emit 0C0h
-        _emit 8Dh
-        _emit 8Eh
-        _emit 10h
-        _emit 02h
-        _emit 00h
-        _emit 00h
-        _emit 0C7h
-        _emit 06h
-        _emit 0C8h
-        _emit 66h
-        _emit 0Bh
-        _emit 01h
-        _emit 89h
-        _emit 01h
-        _emit 89h
-        _emit 41h
-        _emit 04h
-        _emit 89h
-        _emit 44h
-        _emit 24h
-        _emit 10h
-        _emit 89h
-        _emit 41h
-        _emit 08h
-        _emit 8Bh
-        _emit 41h
-        _emit 04h
-        _emit 8Bh
-        _emit 11h
-        _emit 50h
-        _emit 52h
-        _emit 0C6h
-        _emit 44h
-        _emit 24h
-        _emit 18h
-        _emit 01h
-        _emit 0E8h
-        _emit 0BAh
-        _emit 09h
-        _emit 0DCh
-        _emit 0FFh
-        _emit 8Bh
-        _emit 4Ch
-        _emit 24h
-        _emit 08h
-        _emit 8Bh
-        _emit 0C6h
-        _emit 5Eh
-        _emit 64h
-        _emit 89h
-        _emit 0Dh
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 00h
-        _emit 83h
-        _emit 0C4h
-        _emit 10h
-        _emit 0C3h
-    }
+public:
+	BfmeSpecialPowerModuleDataBase();
+	virtual ~BfmeSpecialPowerModuleDataBase();
+
+private:
+	unsigned char m_unmodelled_04[ 0x210 - 4 ];
+};
+
+class PlayerUpgradeRange
+{
+public:
+	void erase( void *first, void *last );
+
+	void *volatile m_begin;
+	void *volatile m_end;
+	void *m_capacity;
+};
+
+class PlayerUpgradeSpecialPowerModuleData : public BfmeSpecialPowerModuleDataBase
+{
+public:
+	PlayerUpgradeSpecialPowerModuleData();
+	virtual ~PlayerUpgradeSpecialPowerModuleData();
+
+private:
+	_STL::vector<AsciiString> m_upgrades; // +0x210
+};
+
+// ??0PlayerUpgradeSpecialPowerModuleData@@QAE@XZ
+PlayerUpgradeSpecialPowerModuleData::PlayerUpgradeSpecialPowerModuleData()
+{
+	PlayerUpgradeRange *range =
+		reinterpret_cast<PlayerUpgradeRange *>( &m_upgrades );
+	void *last = range->m_end;
+	void *first = range->m_begin;
+	range->erase( first, last );
 }
