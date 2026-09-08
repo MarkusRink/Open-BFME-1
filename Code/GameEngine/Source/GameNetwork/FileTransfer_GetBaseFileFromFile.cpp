dc71be7be7116e198af7716dbadb18202db7bfe6
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 08:05:34 -0400

    Convert GetBaseFileFromFile to C++

---
+++ Code/GameEngine/Source/GameNetwork/FileTransfer_GetBaseFileFromFile.cpp
@@ -0,0 +1,76 @@
+// cl: /GX
+
+class AsciiString;
+AsciiString GetBaseFileFromFile(AsciiString fname);
+
+extern "C" void *memcpy(void *destination, const void *source, unsigned int count);
+
+template <typename T>
+class StringBase
+{
+	friend class AsciiString;
+	friend AsciiString GetBaseFileFromFile(AsciiString fname);
+
+	StringBase() : m_data(0) {}
+	StringBase(const StringBase<T> &other);
+	StringBase(const T *str);
+	~StringBase();
+
+public:
+	T *getBufferForRead(int length);
+
+
+private:
+	struct Header
+	{
+		int ref_count;
+		unsigned short length;
+		unsigned short capacity;
+		T data[1];
+	};
+
+	Header *m_data;
+};
+
+class AsciiString : private StringBase<char>
+{
+public:
+	AsciiString() : StringBase<char>() {}
+	AsciiString(const AsciiString &other) : StringBase<char>(other) {}
+	AsciiString(const char *text) : StringBase<char>(text) {}
+	~AsciiString() {}
+	const char *str() const
+	{
+		return m_data ? &m_data->data[0] : (const char *)0x0107388B;
+	}
+	const char *reverseFind(char match) const
+	{
+		const char *first = str();
+		const char *p = first + (m_data ? m_data->length : 0);
+		while (p != first)
+		{
+			--p;
+			if (*p == match)
+				return p;
+		}
+		return 0;
+	}
+
+	static const AsciiString TheEmptyString;
+	friend AsciiString GetBaseFileFromFile(AsciiString fname);
+};
+
+AsciiString GetBaseFileFromFile(AsciiString fname)
+{
+	const char *separator = fname.reverseFind('.');
+	if (separator)
+	{
+		int prefixLength = separator - fname.str();
+		AsciiString base;
+		char *buffer = base.getBufferForRead(prefixLength);
+		memcpy(buffer, fname.str(), prefixLength);
+		buffer[prefixLength] = 0;
+		return buffer;
+	}
+	return AsciiString::TheEmptyString;
+}
