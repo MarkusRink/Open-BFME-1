46891d5891890f410e6b879a23e4ed0ebf364eea
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 08:16:42 -0400

    Convert box emission template clone to C++

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/BoxEmissionVolumeConcreteModuleTemplateCloneThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/BoxEmissionVolumeConcreteModuleTemplateCloneThunk.cpp
@@ -1,39 +1,54 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
+
+// Open-BFME5: BoxEmissionVolume ConcreteModuleTemplate::clone
 
 namespace FXParticleSystem
 {
-template <int N>
-class DefaultParticleModule
-{
-};
-
-template <int N>
-class DefaultParticleModuleTemplate
-{
-};
-
-class BoxEmissionVolumeModule
-{
-};
-
-class BoxEmissionVolumeModuleTemplate
-{
-};
+template <int N> class DefaultParticleModule {};
+template <int N> class DefaultParticleModuleTemplate {};
+class BoxEmissionVolumeModule {};
+class BoxEmissionVolumeModuleTemplate {};
 
 extern const char BOX_EMISSION_VOLUME_MODULE_KEY[1];
 extern const char BOX_EMISSION_VOLUME_MODULE_NAME[1];
 
-template <int Category, const char (&Key)[1], const char (&Name)[1], class Module, class ModuleTemplate, class ParticleModule, class ParticleModuleTemplate>
-class ModuleTag
-{
-};
-
-template <class Tag>
-class ConcreteModuleTemplate;
+template <int Category, const char (&Key)[1], const char (&Name)[1], class Module,
+    class ModuleTemplate, class ParticleModule, class ParticleModuleTemplate>
+class ModuleTag {};
 
 typedef ModuleTag<5, BOX_EMISSION_VOLUME_MODULE_KEY, BOX_EMISSION_VOLUME_MODULE_NAME,
     BoxEmissionVolumeModule, BoxEmissionVolumeModuleTemplate,
     DefaultParticleModule<5>, DefaultParticleModuleTemplate<5> > BoxEmissionVolumeTag;
+
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
+
+class BoxEmissionVolumeTemplateCopyCtorShim
+{
+public:
+    void construct(const void *source);
+};
+
+extern "C" char BoxEmissionVolumeConcrete_vtbl0;
+extern "C" char BoxEmissionVolumeConcrete_vtbl4;
+extern "C" char BoxEmissionVolumeConcrete_vtbl8;
+
+template <class Tag> class ConcreteModuleTemplate;
+
+class BoxEmissionVolumeTemplateAllocation
+{
+public:
+    __forceinline BoxEmissionVolumeTemplateAllocation(const void *source)
+    {
+        ((BoxEmissionVolumeTemplateCopyCtorShim *)this)->construct(source);
+        *(void **)((char *)this + 0) = &BoxEmissionVolumeConcrete_vtbl0;
+        *(void **)((char *)this + 4) = &BoxEmissionVolumeConcrete_vtbl4;
+        *(void **)((char *)this + 8) = &BoxEmissionVolumeConcrete_vtbl8;
+    }
+
+private:
+    unsigned char m_bytes[0x1c];
+};
 
 template <>
 class ConcreteModuleTemplate<BoxEmissionVolumeTag>
@@ -42,67 +57,9 @@ public:
     virtual BoxEmissionVolumeModuleTemplate *clone() const;
 };
 
-__declspec(naked) BoxEmissionVolumeModuleTemplate *ConcreteModuleTemplate<BoxEmissionVolumeTag>::clone() const
+BoxEmissionVolumeModuleTemplate *ConcreteModuleTemplate<BoxEmissionVolumeTag>::clone() const
 {
-    __asm {
-        _emit 056h
-        _emit 057h
-        _emit 06Ah
-        _emit 01Ch
-        _emit 08Bh
-        _emit 0F9h
-        _emit 0E8h
-        _emit 055h
-        _emit 050h
-        _emit 02Ah
-        _emit 000h
-        _emit 08Bh
-        _emit 0F0h
-        _emit 083h
-        _emit 0C4h
-        _emit 004h
-        _emit 085h
-        _emit 0F6h
-        _emit 074h
-        _emit 021h
-        _emit 057h
-        _emit 08Bh
-        _emit 0CEh
-        _emit 0E8h
-        _emit 016h
-        _emit 005h
-        _emit 0A5h
-        _emit 0FFh
-        _emit 05Fh
-        _emit 0C7h
-        _emit 006h
-        _emit 0D4h
-        _emit 011h
-        _emit 011h
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 004h
-        _emit 0D0h
-        _emit 011h
-        _emit 011h
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 008h
-        _emit 0BCh
-        _emit 011h
-        _emit 011h
-        _emit 001h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 0C3h
-        _emit 05Fh
-        _emit 033h
-        _emit 0C0h
-        _emit 05Eh
-        _emit 0C3h
-    }
+    return (BoxEmissionVolumeModuleTemplate *)new BoxEmissionVolumeTemplateAllocation(this);
 }
+
 }
