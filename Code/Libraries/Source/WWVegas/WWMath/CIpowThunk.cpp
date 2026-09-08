7d539d41e03f6a7ff07e1c2983307319856ba498
Jonathan <jonf38@users.noreply.github.com>
2026-09-08 02:50:27 -0400

    wwmath: replace naked CIpow import stub

--- Code/Libraries/Source/WWVegas/WWMath/CIpowThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWMath/CIpowThunk.cpp
@@ -1,11 +1,6 @@
-extern "C" __declspec(naked) void __CIpow()
+extern "C" __declspec(dllimport) void bfmeCIpowImport();
+
+extern "C" void __CIpow()
 {
-    __asm {
-        _emit 0FFh
-        _emit 025h
-        _emit 044h
-        _emit 092h
-        _emit 035h
-        _emit 001h
-    }
+	bfmeCIpowImport();
 }
