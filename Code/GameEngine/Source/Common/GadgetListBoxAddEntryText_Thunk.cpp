a102d659a3ca9cae570b1fe8565cacb1b2b348ec
Gabe Borges <gbborges@proton.me>
2026-09-08 16:52:04 -0400

    feat(gui): convert listbox text insertion
    The retail vtable call and the caller's ListboxData offsets identify this body as GadgetListBoxAddEntryText. The clean body lives in Code/GameEngine/Source/Common/GadgetListBoxAddEntryText_Thunk.cpp and matches the 357-byte retail empty-string replacement, message dispatch, and scroll update.

--- Code/GameEngine/Source/Common/GadgetListBoxAddEntryText_Thunk.cpp
+++ Code/GameEngine/Source/Common/GadgetListBoxAddEntryText_Thunk.cpp
@@ -1,370 +1,193 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
 
-class GameWindow;
-class UnicodeString {};
-int GadgetListBoxAddEntryText(GameWindow *, UnicodeString, int, int, int, bool);
+typedef bool Bool;
+typedef int Int;
+typedef unsigned int UnsignedInt;
+typedef unsigned int WindowMsgData;
 
-// ?GadgetListBoxAddEntryText@@YAHPAVGameWindow@@VUnicodeString@@HHH_N@Z
-__declspec(naked) int GadgetListBoxAddEntryText(GameWindow *, UnicodeString, int, int, int, bool)
+struct UnicodeStringData
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0x10
-		__emit 0x8d
-		__emit 0x02
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xec
-		__emit 0x20
-		__emit 0x55
-		__emit 0x57
-		__emit 0x8b
-		__emit 0x7c
-		__emit 0x24
-		__emit 0x38
-		__emit 0x33
-		__emit 0xed
-		__emit 0x3b
-		__emit 0xfd
-		__emit 0x89
-		__emit 0x6c
-		__emit 0x24
-		__emit 0x30
-		__emit 0x75
-		__emit 0x25
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x3c
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x30
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0xe7
-		__emit 0xcc
-		__emit 0x3c
-		__emit 0x00
-		__emit 0x5f
-		__emit 0x83
-		__emit 0xc8
-		__emit 0xff
-		__emit 0x5d
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x20
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x2c
-		__emit 0xc3
-		__emit 0x8b
-		__emit 0x44
-		__emit 0x24
-		__emit 0x3c
-		__emit 0x3b
-		__emit 0xc5
-		__emit 0x53
-		__emit 0xbb
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x74
-		__emit 0x06
-		__emit 0x66
-		__emit 0x39
-		__emit 0x68
-		__emit 0x04
-		__emit 0x75
-		__emit 0x2e
-		__emit 0x68
-		__emit 0x34
-		__emit 0x4c
-		__emit 0x08
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x40
-		__emit 0xe8
-		__emit 0xc1
-		__emit 0xd8
-		__emit 0x3c
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x44
-		__emit 0x24
-		__emit 0x3c
-		__emit 0x50
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x44
-		__emit 0x88
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x38
-		__emit 0xe8
-		__emit 0xff
-		__emit 0xcf
-		__emit 0x3c
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x3c
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x34
-		__emit 0x00
-		__emit 0xe8
-		__emit 0x91
-		__emit 0xcc
-		__emit 0x3c
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x48
-		__emit 0x8b
-		__emit 0x54
-		__emit 0x24
-		__emit 0x4c
-		__emit 0x89
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x10
-		__emit 0x8a
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x50
-		__emit 0x8d
-		__emit 0x44
-		__emit 0x24
-		__emit 0x40
-		__emit 0x89
-		__emit 0x44
-		__emit 0x24
-		__emit 0x18
-		__emit 0x83
-		__emit 0xc8
-		__emit 0xff
-		__emit 0x88
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x20
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xcf
-		__emit 0x89
-		__emit 0x54
-		__emit 0x24
-		__emit 0x18
-		__emit 0x89
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x20
-		__emit 0x89
-		__emit 0x44
-		__emit 0x24
-		__emit 0x2c
-		__emit 0x89
-		__emit 0x44
-		__emit 0x24
-		__emit 0x28
-		__emit 0xe8
-		__emit 0xc2
-		__emit 0xaf
-		__emit 0xb8
-		__emit 0xff
-		__emit 0x8b
-		__emit 0xf0
-		__emit 0x66
-		__emit 0x8b
-		__emit 0x16
-		__emit 0x66
-		__emit 0x3b
-		__emit 0x56
-		__emit 0x2c
-		__emit 0x8b
-		__emit 0xcf
-		__emit 0x0f
-		__emit 0x9e
-		__emit 0xc0
-		__emit 0x33
-		__emit 0xdb
-		__emit 0x84
-		__emit 0xc0
-		__emit 0x0f
-		__emit 0x94
-		__emit 0xc3
-		__emit 0xe8
-		__emit 0xa8
-		__emit 0xaf
-		__emit 0xb8
-		__emit 0xff
-		__emit 0x3b
-		__emit 0xc5
-		__emit 0x75
-		__emit 0x06
-		__emit 0x89
-		__emit 0x6c
-		__emit 0x24
-		__emit 0x10
-		__emit 0xeb
-		__emit 0x0b
-		__emit 0x8b
-		__emit 0xc8
-		__emit 0xe8
-		__emit 0xff
-		__emit 0xb5
-		__emit 0xff
-		__emit 0xff
-		__emit 0x89
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x8b
-		__emit 0x54
-		__emit 0x24
-		__emit 0x48
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x40
-		__emit 0x1b
-		__emit 0x2f
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x01
-		__emit 0x52
-		__emit 0x8d
-		__emit 0x54
-		__emit 0x24
-		__emit 0x18
-		__emit 0x52
-		__emit 0x68
-		__emit 0x11
-		__emit 0x40
-		__emit 0x00
-		__emit 0x00
-		__emit 0x57
-		__emit 0xff
-		__emit 0x90
-		__emit 0xd4
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8b
-		__emit 0xe8
-		__emit 0x8a
-		__emit 0x46
-		__emit 0x0d
-		__emit 0x84
-		__emit 0xc0
-		__emit 0x5e
-		__emit 0x74
-		__emit 0x23
-		__emit 0x8b
-		__emit 0x54
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x8b
-		__emit 0xc5
-		__emit 0x2b
-		__emit 0xc2
-		__emit 0x3b
-		__emit 0xc3
-		__emit 0x75
-		__emit 0x17
-		__emit 0x57
-		__emit 0xe8
-		__emit 0xae
-		__emit 0xa7
-		__emit 0xb6
-		__emit 0xff
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x04
-		__emit 0x84
-		__emit 0xc0
-		__emit 0x74
-		__emit 0x0a
-		__emit 0x55
-		__emit 0x57
-		__emit 0xe8
-		__emit 0xc4
-		__emit 0xa4
-		__emit 0xb8
-		__emit 0xff
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x08
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x40
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x34
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0xcf
-		__emit 0xcb
-		__emit 0x3c
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x2c
-		__emit 0x5b
-		__emit 0x5f
-		__emit 0x8b
-		__emit 0xc5
-		__emit 0x5d
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x2c
-		__emit 0xc3
+	unsigned int m_refCount;
+	unsigned short m_length;
+};
+
+template <typename BfmeWideChar>
+class StringBase
+{
+	friend class UnicodeString;
+
+private:
+	StringBase(const StringBase<BfmeWideChar> &source);
+	StringBase(const BfmeWideChar *text);
+	~StringBase();
+	void set(const StringBase<BfmeWideChar> &source);
+
+protected:
+	void *m_data;
+};
+
+class UnicodeString : public StringBase<unsigned short>
+{
+public:
+	UnicodeString(const unsigned short *text)
+		: StringBase<unsigned short>(text)
+	{
 	}
+
+	UnicodeString(const UnicodeString &source)
+		: StringBase<unsigned short>(source)
+	{
+	}
+
+	~UnicodeString()
+	{
+	}
+
+	bool isEmpty() const
+	{
+		return m_data == 0 || ((UnicodeStringData *)m_data)->m_length == 0;
+	}
+
+	UnicodeString &operator=(const UnicodeString &source)
+	{
+		set(*(const StringBase<unsigned short> *)&source);
+		return *this;
+	}
+};
+
+class GameWindow
+{
+public:
+	void *winGetUserData();
+};
+
+struct _ListboxData
+{
+	short listLength;
+	short columns;
+	unsigned char m_unmodelled_04[9];
+	Bool scrollIfAtEnd;
+	unsigned char m_unmodelled_0e[0x1e];
+	short endPos;
+};
+
+typedef struct _ListboxData ListboxData;
+
+typedef struct _AddMessageStruct
+{
+	Int row;
+	Int column;
+	const void *data;
+	Int type;
+	Bool overwrite;
+	Int width;
+	Int height;
+} AddMessageStruct;
+
+class GameWindowManager
+{
+public:
+	virtual void _bfme_pad_00() = 0;
+	virtual void _bfme_pad_04() = 0;
+	virtual void _bfme_pad_08() = 0;
+	virtual void _bfme_pad_0C() = 0;
+	virtual void _bfme_pad_10() = 0;
+	virtual void _bfme_pad_14() = 0;
+	virtual void _bfme_pad_18() = 0;
+	virtual void _bfme_pad_1C() = 0;
+	virtual void _bfme_pad_20() = 0;
+	virtual void _bfme_pad_24() = 0;
+	virtual void _bfme_pad_28() = 0;
+	virtual void _bfme_pad_2C() = 0;
+	virtual void _bfme_pad_30() = 0;
+	virtual void _bfme_pad_34() = 0;
+	virtual void _bfme_pad_38() = 0;
+	virtual void _bfme_pad_3C() = 0;
+	virtual void _bfme_pad_40() = 0;
+	virtual void _bfme_pad_44() = 0;
+	virtual void _bfme_pad_48() = 0;
+	virtual void _bfme_pad_4C() = 0;
+	virtual void _bfme_pad_50() = 0;
+	virtual void _bfme_pad_54() = 0;
+	virtual void _bfme_pad_58() = 0;
+	virtual void _bfme_pad_5C() = 0;
+	virtual void _bfme_pad_60() = 0;
+	virtual void _bfme_pad_64() = 0;
+	virtual void _bfme_pad_68() = 0;
+	virtual void _bfme_pad_6C() = 0;
+	virtual void _bfme_pad_70() = 0;
+	virtual void _bfme_pad_74() = 0;
+	virtual void _bfme_pad_78() = 0;
+	virtual void _bfme_pad_7C() = 0;
+	virtual void _bfme_pad_80() = 0;
+	virtual void _bfme_pad_84() = 0;
+	virtual void _bfme_pad_88() = 0;
+	virtual void _bfme_pad_8C() = 0;
+	virtual void _bfme_pad_90() = 0;
+	virtual void _bfme_pad_94() = 0;
+	virtual void _bfme_pad_98() = 0;
+	virtual void _bfme_pad_9C() = 0;
+	virtual void _bfme_pad_A0() = 0;
+	virtual void _bfme_pad_A4() = 0;
+	virtual void _bfme_pad_A8() = 0;
+	virtual void _bfme_pad_AC() = 0;
+	virtual void _bfme_pad_B0() = 0;
+	virtual void _bfme_pad_B4() = 0;
+	virtual void _bfme_pad_B8() = 0;
+	virtual void _bfme_pad_BC() = 0;
+	virtual void _bfme_pad_C0() = 0;
+	virtual void _bfme_pad_C4() = 0;
+	virtual void _bfme_pad_C8() = 0;
+	virtual void _bfme_pad_CC() = 0;
+	virtual void _bfme_pad_D0() = 0;
+	virtual Int winSendSystemMsg(GameWindow *window, UnsignedInt message,
+		WindowMsgData data1, WindowMsgData data2) = 0;
+};
+
+extern GameWindowManager *TheWindowManager;
+
+int __fastcall getListboxBottomEntry(ListboxData *listData);
+bool GadgetListBoxIsFull(GameWindow *window);
+void GadgetListBoxSetBottomVisibleEntry(GameWindow *window, Int newPos);
+
+// The vtable slot, caller data layout, and retail address identify this as
+// the BFME listbox text insertion body.
+Int GadgetListBoxAddEntryText(GameWindow *listbox, UnicodeString text,
+	Int color, Int row, Int column, Bool overwrite)
+{
+	if (!listbox)
+		return -1;
+
+	if (text.isEmpty())
+		text = UnicodeString((const unsigned short *)0x01084c34);
+
+	Int index;
+	AddMessageStruct addInfo;
+	addInfo.row = row;
+	addInfo.column = column;
+	addInfo.type = 1;
+	addInfo.data = &text;
+	addInfo.overwrite = overwrite;
+	addInfo.height = -1;
+	addInfo.width = -1;
+
+	ListboxData *listData = (ListboxData *)listbox->winGetUserData();
+	Bool wasFull = listData->listLength <= listData->endPos;
+	Int newEntryOffset = wasFull ? 0 : 1;
+
+	ListboxData *bottomData = (ListboxData *)listbox->winGetUserData();
+	Int oldBottomIndex;
+	if (bottomData == 0)
+		oldBottomIndex = 0;
+	else
+		oldBottomIndex = getListboxBottomEntry(bottomData);
+
+	index = TheWindowManager->winSendSystemMsg(listbox, 0x4011,
+		(WindowMsgData)&addInfo, (WindowMsgData)color);
+
+	if (listData->scrollIfAtEnd && index - oldBottomIndex == newEntryOffset &&
+		GadgetListBoxIsFull(listbox))
+		GadgetListBoxSetBottomVisibleEntry(listbox, index);
+
+	return index;
 }
